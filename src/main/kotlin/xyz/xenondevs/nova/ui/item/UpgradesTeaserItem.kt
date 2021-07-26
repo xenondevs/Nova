141a5ec010ebe362c7fe9fd9022e592605a061b2
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2021-07-26 22:02:10 +0200

    Localized UpgradesTeaserItem

--- src/main/kotlin/xyz/xenondevs/nova/ui/item/UpgradesTeaserItem.kt
+++ src/main/kotlin/xyz/xenondevs/nova/ui/item/UpgradesTeaserItem.kt
@@ -2,16 +2,18 @@ package xyz.xenondevs.nova.ui.item
 
 import de.studiocode.invui.item.ItemBuilder
 import de.studiocode.invui.item.impl.BaseItem
+import net.md_5.bungee.api.ChatColor
 import org.bukkit.entity.Player
 import org.bukkit.event.inventory.ClickType
 import org.bukkit.event.inventory.InventoryClickEvent
 import xyz.xenondevs.nova.material.NovaMaterial
+import xyz.xenondevs.nova.util.localized
 
 object UpgradesTeaserItem : BaseItem() {
     override fun getItemBuilder(): ItemBuilder {
         return NovaMaterial.UPGRADES_BUTTON.createBasicItemBuilder()
             .setLocalizedName("menu.nova.upgrades")
-            .setLore(listOf("§cComing soon!"))
+            .addLocalizedLoreLines(localized(ChatColor.RED, "menu.nova.upgrades.coming-soon"))
     }
     
     override fun handleClick(clickType: ClickType, player: Player, event: InventoryClickEvent) = Unit
