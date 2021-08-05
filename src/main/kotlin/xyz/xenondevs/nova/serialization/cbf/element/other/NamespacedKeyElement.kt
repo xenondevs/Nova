dc9c3e868a59bcc41c523f4224b93e8cfeb0e1b2
ByteZ1337 <0xbytez@gmail.com>
2021-08-05 20:25:18 +0200

    Added NamespacedKeyElement

---
+++ src/main/kotlin/xyz/xenondevs/nova/serialization/cbf/element/other/NamespacedKeyElement.kt
@@ -0,0 +1,25 @@
+package xyz.xenondevs.nova.serialization.cbf.element.other
+
+import io.netty.buffer.ByteBuf
+import org.bukkit.NamespacedKey
+import xyz.xenondevs.nova.serialization.cbf.BackedElement
+import xyz.xenondevs.nova.serialization.cbf.BinaryDeserializer
+import xyz.xenondevs.nova.util.readString
+import xyz.xenondevs.nova.util.writeString
+
+class NamespacedKeyElement(override val value: NamespacedKey) : BackedElement<NamespacedKey> {
+    override fun getTypeId() = 23.toByte()
+    
+    override fun write(buf: ByteBuf) {
+        buf.writeString(value.namespace)
+        buf.writeString(value.key)
+    }
+}
+
+object NamespacedKeyDeserializer : BinaryDeserializer<NamespacedKeyElement> {
+    override fun read(buf: ByteBuf): NamespacedKeyElement {
+        @Suppress("DEPRECATION") // Other constructor doesn't accept a string namespace
+        return NamespacedKeyElement(NamespacedKey(buf.readString(), buf.readString()))
+    }
+    
+}
