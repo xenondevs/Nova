665e51623132b78e74aebc87a8a526846c53e605
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2021-08-20 12:35:35 +0200

    Improved contentEquals

--- src/main/kotlin/xyz/xenondevs/nova/util/CollectionUtils.kt
+++ src/main/kotlin/xyz/xenondevs/nova/util/CollectionUtils.kt
@@ -1,5 +1,17 @@
 package xyz.xenondevs.nova.util
 
-fun <E> List<E>.contentEquals(other: List<E>) = containsAll(other) && other.containsAll(this)
+fun <E> List<E>.contentEquals(other: List<E>) = size == other.size && containsAll(other)
 
-fun <E> Set<E>.contentEquals(other: Set<E>) = containsAll(other) && other.containsAll(this)
+fun <E> Set<E>.contentEquals(other: Set<E>) = size == other.size && containsAll(other)
+
+fun <E> MutableList<E>.removeFirstWhere(test: (E) -> Boolean): Boolean {
+    val iterator = iterator()
+    while (iterator.hasNext()) {
+        if (test(iterator.next())) {
+            iterator.remove()
+            return true
+        }
+    }
+    
+    return false
+}
