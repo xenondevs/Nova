9c0d51e0e9947c58b20bd8fe081f29d68e3fbe06
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2021-11-20 17:09:05 +0100

    Changed file name storage.json to storage.do-not-edit

--- src/main/kotlin/xyz/xenondevs/nova/data/config/PermanentStorage.kt
+++ src/main/kotlin/xyz/xenondevs/nova/data/config/PermanentStorage.kt
@@ -1,6 +1,7 @@
 package xyz.xenondevs.nova.data.config
 
 import com.google.gson.JsonObject
+import xyz.xenondevs.nova.NOVA
 import xyz.xenondevs.nova.util.data.GSON
 import xyz.xenondevs.nova.util.data.JSON_PARSER
 import xyz.xenondevs.nova.util.data.fromJson
@@ -8,8 +9,18 @@ import java.io.File
 
 object PermanentStorage {
     
-    private val file = File("plugins/Nova/storage.json").apply { parentFile.mkdirs() }
-    val mainObj: JsonObject = if (file.exists()) JSON_PARSER.parse(file.reader()).asJsonObject else JsonObject()
+    private val file: File = File("${NOVA.dataFolder}/storage.do-not-edit").also { it.parentFile.mkdirs() }
+    val mainObj: JsonObject
+    
+    init {
+        val legacyFile = File("${NOVA.dataFolder}/storage.json")
+        if (!file.exists() && legacyFile.exists())
+            legacyFile.renameTo(file)
+        
+        mainObj = if (file.exists())
+            JSON_PARSER.parse(file.reader()).asJsonObject
+        else JsonObject()
+    }
     
     fun store(key: String, data: Any) {
         mainObj.add(key, GSON.toJsonTree(data))
