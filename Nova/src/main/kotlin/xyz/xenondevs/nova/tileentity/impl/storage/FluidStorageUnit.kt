042fae359e7b20a1c8fc669ba091385f198f1436
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2022-01-14 22:15:21 +0100

    Update FluidStorageUnit.kt

--- Nova/src/main/kotlin/xyz/xenondevs/nova/tileentity/impl/storage/FluidStorageUnit.kt
+++ Nova/src/main/kotlin/xyz/xenondevs/nova/tileentity/impl/storage/FluidStorageUnit.kt
@@ -6,10 +6,6 @@ import de.studiocode.invui.gui.builder.guitype.GUIType
 import de.studiocode.invui.item.ItemProvider
 import de.studiocode.invui.item.builder.ItemBuilder
 import de.studiocode.invui.item.impl.BaseItem
-import de.studiocode.invui.virtualinventory.event.ItemUpdateEvent
-import net.md_5.bungee.api.ChatColor
-import net.md_5.bungee.api.chat.TextComponent
-import net.md_5.bungee.api.chat.TranslatableComponent
 import net.minecraft.world.entity.EquipmentSlot
 import org.bukkit.Material
 import org.bukkit.entity.Player
@@ -27,10 +23,8 @@ import xyz.xenondevs.nova.tileentity.network.fluid.holder.NovaFluidHolder
 import xyz.xenondevs.nova.ui.FluidBar
 import xyz.xenondevs.nova.ui.config.side.OpenSideConfigItem
 import xyz.xenondevs.nova.ui.config.side.SideConfigGUI
-import xyz.xenondevs.nova.util.runTaskLater
 import xyz.xenondevs.nova.world.armorstand.FakeArmorStand
 import java.util.*
-
 
 private val MAX_CAPACITY = NovaConfig[FLUID_STORAGE_UNIT].getLong("max_capacity")!!
 
@@ -43,10 +37,13 @@ class FluidStorageUnit(
 ) : NetworkedTileEntity(uuid, data, material, ownerUUID, armorStand) {
     
     override val gui = lazy(::FluidStorageUnitGUI)
-    val fluidTank = getFluidContainer("fluid", setOf(FluidType.LAVA, FluidType.WATER), MAX_CAPACITY, 0, ::handleFluidUpdate)
-    val inventory = getInventory("inventory", 1, ::handleInventoryUpdate)
+    private val fluidTank = getFluidContainer("fluid", setOf(FluidType.LAVA, FluidType.WATER), MAX_CAPACITY, 0, ::handleFluidUpdate)
     private val fluidLevel = FakeArmorStand(armorStand.location) { it.isInvisible = true; it.isMarker = true }
     override val fluidHolder = NovaFluidHolder(this, fluidTank to NetworkConnectionType.BUFFER) { createSideConfig(NetworkConnectionType.BUFFER) }
+    
+    init {
+        handleFluidUpdate()
+    }
     
     private fun handleFluidUpdate() {
         val stack = if (fluidTank.hasFluid()) {
@@ -56,28 +53,9 @@ class FluidStorageUnit(
                 else -> throw IllegalStateException()
             }.item.createItemStack(10)
         } else null
+        
         fluidLevel.setEquipment(EquipmentSlot.HEAD, stack)
         fluidLevel.updateEquipment()
-    }
-    
-    private fun handleInventoryUpdate(event: ItemUpdateEvent) {
-        if (event.isAdd) {
-            if (!(event.newItemStack.type == Material.LAVA_BUCKET || event.newItemStack.type == Material.WATER_BUCKET)) {
-                event.isCancelled = true
-                return
-            }
-            val fluidType = FluidType.values().first { it.bucket?.type == event.newItemStack.type }
-            if (!fluidTank.accepts(fluidType, 1000)) {
-                event.isCancelled = true
-                return
-            }
-            fluidTank.addFluid(fluidType, 1000)
-            event.newItemStack.type = Material.BUCKET
-            runTaskLater(1) {
-                gui.value.update()
-            }
-            
-        }
     }
     
     override fun handleRemoved(unload: Boolean) {
@@ -86,45 +64,40 @@ class FluidStorageUnit(
     }
     
     inner class FluidStorageUnitGUI : TileEntityGUI() {
+        
         private val sideConfigGUI = SideConfigGUI(
             this@FluidStorageUnit,
             fluidContainers = listOf(fluidTank to "container.nova.fluid_tank"),
             openPrevious = ::openWindow
         )
-        private val fluidStorageDisplay = FluidStorageUnitDisplay(this@FluidStorageUnit)
         
         override val gui: GUI = GUIBuilder(GUIType.NORMAL, 9, 5)
             .setStructure("" +
                 "1 - - - - - - - 2" +
-                "| # # # # # f # |" +
-                "| # i # c # f s |" +
-                "| # # # # # f # |" +
+                "| s # # # # # f |" +
+                "| # # # d # # f |" +
+                "| # # # # # # f |" +
                 "3 - - - - - - - 4")
             .addIngredient('s', OpenSideConfigItem(sideConfigGUI))
-            .addIngredient('i', inventory)
-            .addIngredient('c', fluidStorageDisplay)
+            .addIngredient('d', FluidStorageUnitDisplay())
             .addIngredient('f', FluidBar(3, fluidHolder, fluidTank))
             .build()
         
-        fun update() {
-            fluidStorageDisplay.notifyWindows()
-        }
-        
-        private inner class FluidStorageUnitDisplay(val fluidStorageUnit: FluidStorageUnit) : BaseItem() {
+        private inner class FluidStorageUnitDisplay : BaseItem() {
+            
+            init {
+                fluidTank.updateHandlers += { notifyWindows() }
+            }
             
             override fun getItemProvider(): ItemProvider {
-                
-                val type = fluidStorageUnit.fluidTank.type?.bucket
+                val type = fluidTank.type?.bucket
                     ?: return ItemBuilder(Material.BARRIER).setDisplayName("§r")
-                val amount = fluidStorageUnit.fluidTank.amount
-                val component = TranslatableComponent(
-                    "menu.nova.fluid_storage_unit.item_display_" + if (amount > 1) "plural" else "singular",
-                    TextComponent(amount.toString()).apply { color = ChatColor.GREEN }
-                )
-                return ItemBuilder(type).setDisplayName(component).setAmount(1)
+                val amount = fluidTank.amount
+                return ItemBuilder(type).setDisplayName("§a$amount §7mB").setAmount(1)
             }
             
             override fun handleClick(clickType: ClickType, player: Player, event: InventoryClickEvent) = Unit
+            
         }
     }
     
