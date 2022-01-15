79031b66f8027155aa9909df684e32dec6fde848
ByteZ1337 <0xbytez@gmail.com>
2022-01-15 13:42:49 +0100

    Update Nova.kt

--- Nova/src/main/kotlin/xyz/xenondevs/nova/Nova.kt
+++ Nova/src/main/kotlin/xyz/xenondevs/nova/Nova.kt
@@ -1,7 +1,6 @@
 package xyz.xenondevs.nova
 
 import org.bstats.bukkit.Metrics
-import org.bukkit.event.Listener
 import org.bukkit.plugin.PluginManager
 import org.bukkit.plugin.java.JavaPlugin
 import xyz.xenondevs.nova.api.protection.ProtectionIntegration
@@ -62,7 +61,7 @@ class Nova : JavaPlugin(), INova {
     }
     
     override fun registerProtectionIntegration(integration: ProtectionIntegration) {
-        ProtectionManager.integrations += integration
+        ProtectionManager.integrations.apply { add(size - 1, integration) }
     }
     
 }
