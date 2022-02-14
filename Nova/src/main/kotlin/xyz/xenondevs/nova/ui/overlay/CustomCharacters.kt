b257b1f27d51087169ac7496c66809d2da2c36bb
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2022-02-14 19:07:39 +0100

    Update CustomCharacters.kt

--- Nova/src/main/kotlin/xyz/xenondevs/nova/ui/overlay/CustomCharacters.kt
+++ Nova/src/main/kotlin/xyz/xenondevs/nova/ui/overlay/CustomCharacters.kt
@@ -4,15 +4,33 @@ import net.md_5.bungee.api.ChatColor
 import net.md_5.bungee.api.chat.BaseComponent
 import net.md_5.bungee.api.chat.ComponentBuilder
 import net.md_5.bungee.api.chat.TranslatableComponent
+import xyz.xenondevs.nova.data.resources.Resources
+import xyz.xenondevs.nova.data.resources.builder.GUIData
 import xyz.xenondevs.nova.util.data.getResourceAsStream
 import kotlin.math.abs
 
-enum class GUITexture(private val char: Char, private val width: Int) {
+object CoreGUITexture {
     
-    EMPTY_GUI('\uF000', 176),
-    ELECTRIC_BREWING_STAND('\uF200', 176),
-    CONFIGURE_POTION('\uF201', 176),
-    PICK_COLOR('\uF202', 176);
+    val EMPTY_GUI = GUITexture.of("nova:empty")
+    val SEARCH = GUITexture.of("nova:search")
+    val ITEMS_0 = GUITexture.of("nova:items_0")
+    val ITEMS_1 = GUITexture.of("nova:items_1")
+    val ITEMS_2 = GUITexture.of("nova:items_2")
+    val ITEMS_3 = GUITexture.of("nova:items_3")
+    val ITEMS_4 = GUITexture.of("nova:items_4")
+    val RECIPE_CRAFTING = GUITexture.of("nova:recipe_crafting")
+    val RECIPE_SMELTING = GUITexture.of("nova:recipe_smelting")
+    val RECIPE_SMITHING = GUITexture.of("nova:recipe_smithing")
+    val RECIPE_CONVERSION = GUITexture.of("nova:recipe_conversion")
+    
+}
+
+class GUITexture(private val data: GUIData) {
+    
+    val component: BaseComponent = ComponentBuilder(data.char.toString())
+        .font("nova:gui")
+        .color(ChatColor.WHITE)
+        .create()[0]
     
     fun getTitle(translate: String): Array<BaseComponent> {
         return getTitle(TranslatableComponent(translate))
@@ -20,11 +38,11 @@ enum class GUITexture(private val char: Char, private val width: Int) {
     
     fun getTitle(title: TranslatableComponent): Array<BaseComponent> {
         return ComponentBuilder()
-            .append(CustomCharacters.getMovingComponent(-8)) // move to side to place overlay
-            .append(char.toString())
+            .append(MoveCharacters.getMovingComponent(-8)) // move to side to place overlay
+            .append(data.char.toString())
             .font("nova:gui")
             .color(ChatColor.WHITE)
-            .append(CustomCharacters.getMovingComponent(-width + 7)) // move back to start
+            .append(MoveCharacters.getMovingComponent(-data.width + 7)) // move back to start
             .append(title)
             .font("default")
             .color(ChatColor.DARK_GRAY)
@@ -32,67 +50,45 @@ enum class GUITexture(private val char: Char, private val width: Int) {
         
     }
     
-}
-
-enum class CustomCharacters(fontName: String, char: Char) {
-    
-    // TODO: move these to GUITexture
-    EMPTY_GUI("gui", '\uF000'),
-    CRAFTING_RECIPE("gui", '\uF001'),
-    FURNACE_RECIPE("gui", '\uF002'),
-    CONVERSION_RECIPE("gui", '\uF003'),
-    SMITHING_TABLE("gui", '\uF004'),
-    PULVERIZER_RECIPE("gui", '\uF005'),
-    PRESS_RECIPE("gui", '\uF006'),
-    FLUID_INFUSER("gui", '\uF007'),
-    STAR_COLLECTOR("gui", '\uF008'),
-    COBBLESTONE_GENERATOR("gui", '\uF009'),
-    FREEZER("gui", '\uF00A'),
-    SEARCH("gui", '\uF00B'),
-    CREATIVE_0("gui", '\uF100'),
-    CREATIVE_1("gui", '\uF101'),
-    CREATIVE_2("gui", '\uF102'),
-    CREATIVE_3("gui", '\uF103'),
-    CREATIVE_4("gui", '\uF104');
-    
-    val component: BaseComponent = ComponentBuilder(char.toString())
-        .font("nova:$fontName")
-        .color(ChatColor.WHITE)
-        .create()[0]
-    
     companion object {
         
-        private val componentCache = HashMap<Int, BaseComponent>()
-        private val charWidths = getResourceAsStream("char_widths.bin")!!.readAllBytes()
-        
-        private fun getMovingString(distance: Int): String {
-            val start = if (distance < 0) '\uF000'.code else '\uF100'.code
-            val num = abs(distance)
-            val buffer = StringBuffer()
-            for (bit in 0 until 31)
-                if (num and (1 shl bit) != 0)
-                    buffer.append((start + bit).toChar())
-            
-            return buffer.toString()
-        }
-        
-        fun getMovingComponent(distance: Int): BaseComponent {
-            return componentCache.getOrPut(distance) {
-                ComponentBuilder(getMovingString(distance))
-                    .font("nova:move")
-                    .create()[0]
-            }
-        }
-        
-        fun getStringLength(string: String): Int {
-            var size = 0
-            string.toCharArray().forEach { size += getCharWidth(it) }
-            return size
-        }
-        
-        private fun getCharWidth(char: Char): Int =
-            charWidths[char.code].toInt()
+        fun of(id: String) = GUITexture(Resources.getGUIData(id))
         
     }
+    
+}
+
+object MoveCharacters {
+    
+    private val componentCache = HashMap<Int, BaseComponent>()
+    private val charWidths = getResourceAsStream("char_widths.bin")!!.readAllBytes()
+    
+    private fun getMovingString(distance: Int): String {
+        val start = if (distance < 0) '\uF000'.code else '\uF100'.code
+        val num = abs(distance)
+        val buffer = StringBuffer()
+        for (bit in 0 until 31)
+            if (num and (1 shl bit) != 0)
+                buffer.append((start + bit).toChar())
+        
+        return buffer.toString()
+    }
+    
+    fun getMovingComponent(distance: Int): BaseComponent {
+        return componentCache.getOrPut(distance) {
+            ComponentBuilder(getMovingString(distance))
+                .font("nova:move")
+                .create()[0]
+        }
+    }
+    
+    fun getStringLength(string: String): Int {
+        var size = 0
+        string.toCharArray().forEach { size += getCharWidth(it) }
+        return size
+    }
+    
+    private fun getCharWidth(char: Char): Int =
+        charWidths[char.code].toInt()
     
 }
