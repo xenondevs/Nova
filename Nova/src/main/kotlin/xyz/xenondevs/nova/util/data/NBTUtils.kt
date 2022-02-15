d3f06cc9d04b514276c9d9bfd8716fe6d897febd
ByteZ1337 <0xbytez@gmail.com>
2022-02-15 21:49:46 +0100

    Added tag ids in NBTUtils

--- Nova/src/main/kotlin/xyz/xenondevs/nova/util/data/NBTUtils.kt
+++ Nova/src/main/kotlin/xyz/xenondevs/nova/util/data/NBTUtils.kt
@@ -4,6 +4,21 @@ import net.minecraft.nbt.*
 
 object NBTUtils {
     
+    var TAG_END = 0
+    var TAG_BYTE = 1
+    var TAG_SHORT = 2
+    var TAG_INT = 3
+    var TAG_LONG = 4
+    var TAG_FLOAT = 5
+    var TAG_DOUBLE = 6
+    var TAG_BYTE_ARRAY = 7
+    var TAG_STRING = 8
+    var TAG_LIST = 9
+    var TAG_COMPOUND = 10
+    var TAG_INT_ARRAY = 11
+    var TAG_LONG_ARRAY = 12
+    var TAG_ANY_NUMERIC = 99
+    
     fun createDoubleList(vararg doubles: Double): ListTag {
         val listTag = ListTag()
         doubles.forEach { listTag.add(DoubleTag.valueOf(it)) }
