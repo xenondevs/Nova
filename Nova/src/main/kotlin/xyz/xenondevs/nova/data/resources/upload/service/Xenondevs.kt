d898e840a818047fde590fa869c469cf88599435
ByteZ1337 <0xbytez@gmail.com>
2022-03-26 09:34:13 +0100

    Adjust to xenondevs upload api changes

--- Nova/src/main/kotlin/xyz/xenondevs/nova/data/resources/upload/service/Xenondevs.kt
+++ Nova/src/main/kotlin/xyz/xenondevs/nova/data/resources/upload/service/Xenondevs.kt
@@ -1,10 +1,16 @@
 package xyz.xenondevs.nova.data.resources.upload.service
 
 import com.google.gson.JsonObject
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.http.ContentType.Application.Zip
+import xyz.xenondevs.nova.HTTP_CLIENT
 import xyz.xenondevs.nova.data.config.JsonConfig
 import xyz.xenondevs.nova.data.resources.upload.UploadService
-import xyz.xenondevs.nova.util.data.HttpMultipartRequest
 import xyz.xenondevs.nova.util.data.getString
+import xyz.xenondevs.nova.util.data.http.BinaryBufferedBody
 import java.io.File
 
 object Xenondevs : UploadService {
@@ -19,15 +25,18 @@ object Xenondevs : UploadService {
             ?: throw IllegalArgumentException("No key specified for xenondevs upload service")
     }
     
-    override fun upload(file: File): String {
+    override suspend fun upload(file: File): String {
         println("uploading")
-        val request = HttpMultipartRequest(API_URL, "PUT") { setRequestProperty("key", key) }
-        request.addFormFile("pack", file, "pack.zip")
-        val response = request.complete()
-        val json = response.jsonResponse as JsonObject
-        check(response.isSuccessful) {
-            "Failed to upload pack to xenondevs: ${response.statusCode} ${json.getString("error")}." +
-                "Please remember that this feature is only available for Patrons!"
+        val json = HTTP_CLIENT.put<HttpStatement>(API_URL) {
+            header("key", key)
+            body = BinaryBufferedBody(file.inputStream(), contentType = Zip)
+        }.execute { response ->
+            val json = response.receive<JsonObject>()
+            check(response.status.isSuccess()) {
+                "Failed to upload pack to xenondevs: ${response.status} ${json.getString("error")}." +
+                    "Please remember that this feature is only available for Patrons!"
+            }
+            return@execute json
         }
         val url = json.getString("url")
         checkNotNull(url) { "Server did not return a url" }
