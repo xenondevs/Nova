5c452b689f2727830d9b9bb3c3d92142d9ba5db3
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2022-04-29 11:54:53 +0200

    Internal ability type constructor

--- Nova/src/main/kotlin/xyz/xenondevs/nova/player/ability/AbilityType.kt
+++ Nova/src/main/kotlin/xyz/xenondevs/nova/player/ability/AbilityType.kt
@@ -1,13 +1,31 @@
 package xyz.xenondevs.nova.player.ability
 
 import org.bukkit.entity.Player
+import xyz.xenondevs.nova.addon.Addon
+import xyz.xenondevs.nova.data.NamespacedId
+import xyz.xenondevs.nova.util.addNamespace
 
-private val ID_PATTERN = Regex("""^[a-z][a-z0-9_]*:[a-z][a-z0-9_]*$""")
-
-class AbilityType<T : Ability>(val id: String, val createAbility: (Player) -> T) {
+object AbilityTypeRegistry {
     
-    init {
-        require(id.matches(ID_PATTERN)) { "Ability id $id does not match pattern $ID_PATTERN" }
+    private val abilityTypes = HashMap<String, AbilityType<*>>()
+    
+    fun <T : Ability> registerAbility(addon: Addon, name: String, createAbility: (Player) -> T): AbilityType<T> {
+        val id = NamespacedId(addon.description.id, name)
+        val ability = AbilityType(id, createAbility)
+        abilityTypes[id.toString()] = ability
+        return ability
+    }
+    
+    fun <T : AbilityType<*>> getAbilityType(addon: Addon, name: String): T? {
+        val id = name.addNamespace(addon.description.id)
+        return getAbilityType(id)
+    }
+    
+    @Suppress("UNCHECKED_CAST")
+    fun <T : AbilityType<*>> getAbilityType(id: String): T? {
+        return abilityTypes[id] as? T
     }
     
 }
+
+class AbilityType<T : Ability> internal constructor(val id: NamespacedId, val createAbility: (Player) -> T)
