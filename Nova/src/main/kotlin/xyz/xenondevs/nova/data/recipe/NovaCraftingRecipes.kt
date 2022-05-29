f26070636c2fd88b5618900e05923353a4ea6f68
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2022-05-29 10:21:07 +0200

    Update NovaCraftingRecipes.kt

--- Nova/src/main/kotlin/xyz/xenondevs/nova/data/recipe/NovaCraftingRecipes.kt
+++ Nova/src/main/kotlin/xyz/xenondevs/nova/data/recipe/NovaCraftingRecipes.kt
@@ -29,7 +29,7 @@ class NovaShapedRecipe(private val optimizedRecipe: OptimizedShapedRecipe) : Sha
         return container.width == width && container.height == height &&
             container.contents.withIndex().all { (index, matrixStack) ->
                 // check if the item stack matches with the given recipe choice
-                val choice = optimizedRecipe.choices[index] ?: return@all matrixStack.isEmpty
+                val choice = optimizedRecipe.choiceMatrix[index] ?: return@all matrixStack.isEmpty
                 return@all matrixStack != null && choice.test(matrixStack.bukkitStack)
             }
     }
