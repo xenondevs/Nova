0498d8c655ff0373ee838be8548785936cf3c609
ByteZ1337 <0xbytez@gmail.com>
2022-06-17 13:04:53 +0200

    Update FakeOnlinePlayer.kt

--- Nova/src/main/kotlin/xyz/xenondevs/nova/integration/protection/FakeOnlinePlayer.kt
+++ Nova/src/main/kotlin/xyz/xenondevs/nova/integration/protection/FakeOnlinePlayer.kt
@@ -1435,11 +1435,11 @@ class FakeOnlinePlayer(
         throw UnsupportedOperationException("Player is not online")
     }
     
-    override fun <T : Projectile?> launchProjectile(projectile: Class<out T?>): T {
+    override fun <T : Projectile> launchProjectile(projectile: Class<out T?>): T {
         throw UnsupportedOperationException("Player is not online")
     }
     
-    override fun <T : Projectile?> launchProjectile(projectile: Class<out T?>, velocity: Vector?): T {
+    override fun <T : Projectile> launchProjectile(projectile: Class<out T?>, velocity: Vector?): T {
         throw UnsupportedOperationException("Player is not online")
     }
     
