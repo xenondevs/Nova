fef4c698347a9bdc135da194d92a6fb75b379d0a
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2022-08-27 17:50:51 +0200

    Update CI

--- .github/scripts/buildtools.sh
+++ .github/scripts/buildtools.sh
@@ -2,7 +2,15 @@
 
 buildtools_dir=~/buildtools
 buildtools=$buildtools_dir/BuildTools.jar
-spigot_version=$(grep -oP '<spigot.version>\K.*?(?=-)' ./pom.xml)
+spigot_regex='version\("spigot", "([0-9.]*)'
+
+while read line
+do
+  if [[ $line =~ $spigot_regex ]]
+  then
+    spigot_version=${BASH_REMATCH[1]}
+  fi 
+done < ./settings.gradle.kts
 
 set -e
 echo "Installing Spigot: $spigot_version"
