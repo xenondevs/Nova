e55d9867f7afcd2b3193b48e1602247ca9adcdce
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2022-09-10 13:34:04 +0200

    Fix PatchedClassLoader

--- nova/src/main/kotlin/xyz/xenondevs/nova/transformer/PatchedClassLoader.kt
+++ nova/src/main/kotlin/xyz/xenondevs/nova/transformer/PatchedClassLoader.kt
@@ -4,22 +4,21 @@ import xyz.xenondevs.nova.NOVA
 import xyz.xenondevs.nova.util.reflection.ReflectionUtils
 import java.lang.reflect.InvocationTargetException
 
-private val PLUGIN_CLASS_LOADER = NOVA.loader.javaClass.classLoader
-
-internal class PatchedClassLoader : ClassLoader(PLUGIN_CLASS_LOADER.parent.parent) {
+internal class PatchedClassLoader : ClassLoader(NOVA.loader.javaClass.classLoader.parent.parent) {
     
+    private val novaClassLoader = NOVA.javaClass.classLoader
     private val findClass = ReflectionUtils.getMethod(ClassLoader::class.java, true, "findClass", String::class.java)
     
     override fun loadClass(name: String, resolve: Boolean): Class<*> {
         if (name.startsWith("xyz.xenondevs.nova")) {
             try {
-                return findClass.invoke(PLUGIN_CLASS_LOADER, name) as Class<*>
+                return findClass.invoke(novaClassLoader, name) as Class<*>
             } catch (e: InvocationTargetException) {
                 throw e.cause ?: Exception()
             }
         }
         
-        return super.loadClass(name, resolve)
+        return parent.loadClass(name)
     }
     
 }
