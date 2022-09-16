13c52e1729310bc2ce638cb4d882e7f48e023272
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2022-09-16 17:14:25 +0200

    Improved TileEntityLimits
    - Added ``amount_per_chunk`` option
    - Added '*' option for amount limiters (limit for all tile-entity types)

--- nova/src/main/kotlin/xyz/xenondevs/nova/world/block/limits/BlockLimiter.kt
+++ nova/src/main/kotlin/xyz/xenondevs/nova/world/block/limits/BlockLimiter.kt
@@ -4,20 +4,23 @@ import org.bukkit.configuration.ConfigurationSection
 import xyz.xenondevs.nova.data.NamespacedId
 import xyz.xenondevs.nova.util.item.novaMaterial
 import xyz.xenondevs.nova.world.block.context.BlockPlaceContext
+import xyz.xenondevs.nova.world.block.limits.BlockLimiter.Companion.ALLOWED
+import xyz.xenondevs.nova.world.block.limits.TileEntityLimits.PlaceResult
 
 internal interface BlockLimiter {
     
-    val denyMessage: String
-    
-    fun canPlace(ctx: BlockPlaceContext): Boolean
+    fun canPlace(ctx: BlockPlaceContext): PlaceResult
     
     companion object {
-        
+    
+        val ALLOWED = PlaceResult(true, "")
+    
         private val limiterTypes: Map<String, (ConfigurationSection) -> BlockLimiter> = hashMapOf(
             "world" to ::WorldBlacklist,
             "type_world" to ::TypeWorldBlacklist,
-            "amount" to { AmountLimiter(it, false) },
-            "amount_per_world" to { AmountLimiter(it, true) }
+            "amount" to { AmountLimiter(it, AmountLimiter.Type.GLOBAL) },
+            "amount_per_world" to { AmountLimiter(it, AmountLimiter.Type.PER_WORLD) },
+            "amount_per_chunk" to { AmountLimiter(it, AmountLimiter.Type.PER_CHUNK) }
         )
         
         fun createNew(type: String, cfg: ConfigurationSection): BlockLimiter? =
@@ -27,47 +30,84 @@ internal interface BlockLimiter {
     
 }
 
-internal class WorldBlacklist(cfg: ConfigurationSection) : BlockLimiter {
+internal abstract class SimpleBlockLimiter(denyMessage: String) : BlockLimiter {
+    
+    private val denied = PlaceResult(false, denyMessage)
+    
+    final override fun canPlace(ctx: BlockPlaceContext): PlaceResult {
+        return if (testPlace(ctx)) ALLOWED else denied
+    }
+    
+    abstract fun testPlace(ctx: BlockPlaceContext): Boolean
+    
+}
+
+
+internal class WorldBlacklist(cfg: ConfigurationSection) : SimpleBlockLimiter("nova.tile_entity_limits.world_blacklist.deny") {
     
     private val blacklist = cfg.getStringList("worlds").toHashSet()
-    override val denyMessage = "nova.tile_entity_limits.world_blacklist.deny"
     
-    override fun canPlace(ctx: BlockPlaceContext): Boolean {
+    override fun testPlace(ctx: BlockPlaceContext): Boolean {
         return ctx.pos.world.name !in blacklist
     }
     
 }
 
-internal class TypeWorldBlacklist(cfg: ConfigurationSection) : BlockLimiter {
+internal class TypeWorldBlacklist(cfg: ConfigurationSection) : SimpleBlockLimiter("nova.tile_entity_limits.world_type_blacklist.deny") {
     
     private val blacklist: Map<String, Set<NamespacedId>> =
         cfg.getKeys(false).associateWithTo(HashMap()) { world -> cfg.getStringList(world).mapTo(HashSet(), NamespacedId::of) }
     
-    override val denyMessage = "nova.tile_entity_limits.world_type_blacklist.deny"
-    
-    override fun canPlace(ctx: BlockPlaceContext): Boolean {
+    override fun testPlace(ctx: BlockPlaceContext): Boolean {
         val id = ctx.item.novaMaterial?.id ?: return true
         return blacklist[ctx.pos.world.name]?.contains(id) != true
     }
     
 }
 
-internal class AmountLimiter(cfg: ConfigurationSection, private val perWorld: Boolean) : BlockLimiter {
+internal class AmountLimiter(cfg: ConfigurationSection, private val type: Type) : BlockLimiter {
     
-    private val limits: Map<NamespacedId, Int> =
-        cfg.getKeys(false).associateTo(HashMap()) { id -> NamespacedId.of(id) to cfg.getInt(id) }
+    private val limits: Map<NamespacedId?, Int> =
+        cfg.getKeys(false).associateTo(HashMap()) { id -> (if (id == "*") null else NamespacedId.of(id)) to cfg.getInt(id) }
     
-    override val denyMessage = "nova.tile_entity_limits.amount" + (if (perWorld) "_per_world" else "") + ".deny"
+    private val deniedSpecific = PlaceResult(false, "nova.tile_entity_limits.amount_${type.name.lowercase()}.deny")
+    private val deniedTotal = PlaceResult(false, "nova.tile_entity_limits.amount_${type.name.lowercase()}_total.deny")
     
-    override fun canPlace(ctx: BlockPlaceContext): Boolean {
-        val id = ctx.item.novaMaterial?.id ?: return true
-        val limit = limits[id] ?: return true
+    override fun canPlace(ctx: BlockPlaceContext): PlaceResult {
+        val id = ctx.item.novaMaterial?.id ?: return ALLOWED
         
-        val amount = if (perWorld)
-            TileEntityTracker.getBlocksPlacedAmount(ctx.ownerUUID, ctx.pos.world.uid, id)
-        else TileEntityTracker.getBlocksPlacedAmount(ctx.ownerUUID, id)
+        val specificLimit = limits[id]
+        val totalLimit = limits[null]
         
-        return limit > amount
+        if (specificLimit != null) {
+            val amount = when (type) {
+                Type.GLOBAL -> TileEntityTracker.getBlocksPlacedAmount(ctx.ownerUUID, id)
+                Type.PER_WORLD -> TileEntityTracker.getBlocksPlacedAmount(ctx.ownerUUID, ctx.pos.world.uid, id)
+                Type.PER_CHUNK -> TileEntityTracker.getBlocksPlacedAmount(ctx.ownerUUID, ctx.pos.chunkPos, id)
+            }
+            
+            if (amount >= specificLimit)
+                return deniedSpecific
+        }
+        
+        if (totalLimit != null) {
+            val amount = when (type) {
+                Type.GLOBAL -> TileEntityTracker.getBlocksPlacedAmount(ctx.ownerUUID)
+                Type.PER_WORLD -> TileEntityTracker.getBlocksPlacedAmount(ctx.ownerUUID, ctx.pos.world.uid)
+                Type.PER_CHUNK -> TileEntityTracker.getBlocksPlacedAmount(ctx.ownerUUID, ctx.pos.chunkPos)
+            }
+            
+            if (amount >= totalLimit)
+                return deniedTotal
+        }
+        
+        return ALLOWED
+    }
+    
+    enum class Type {
+        GLOBAL,
+        PER_WORLD,
+        PER_CHUNK
     }
     
 }
--- nova/src/main/kotlin/xyz/xenondevs/nova/world/block/limits/TileEntityTracker.kt
+++ nova/src/main/kotlin/xyz/xenondevs/nova/world/block/limits/TileEntityTracker.kt
@@ -6,6 +6,8 @@ import xyz.xenondevs.nova.data.config.PermanentStorage
 import xyz.xenondevs.nova.material.TileEntityNovaMaterial
 import xyz.xenondevs.nova.tileentity.TileEntity
 import xyz.xenondevs.nova.util.runTaskTimer
+import xyz.xenondevs.nova.world.BlockPos
+import xyz.xenondevs.nova.world.ChunkPos
 import xyz.xenondevs.nova.world.block.context.BlockBreakContext
 import xyz.xenondevs.nova.world.block.context.BlockPlaceContext
 import java.lang.Integer.max
@@ -17,6 +19,8 @@ internal object TileEntityTracker {
         PermanentStorage.retrieve("block_counter", ::HashMap)
     private val BLOCK_WORLD_COUNTER: HashMap<UUID, HashMap<UUID, HashMap<NamespacedId, Int>>> =
         PermanentStorage.retrieve("block_world_counter", ::HashMap)
+    private val BLOCK_CHUNK_COUNTER: HashMap<UUID, HashMap<ChunkPos, HashMap<NamespacedId, Int>>> =
+        PermanentStorage.retrieve("block_chunk_counter", ::HashMap)
     
     init {
         runTaskTimer(20 * 60, 20 * 60, ::saveCounters)
@@ -26,22 +30,26 @@ internal object TileEntityTracker {
     private fun saveCounters() {
         PermanentStorage.store("block_counter", BLOCK_COUNTER)
         PermanentStorage.store("block_world_counter", BLOCK_WORLD_COUNTER)
+        PermanentStorage.store("block_chunk_counter", BLOCK_CHUNK_COUNTER)
     }
     
     internal fun handleBlockPlace(material: TileEntityNovaMaterial, ctx: BlockPlaceContext) {
-        modifyCounters(ctx.ownerUUID, ctx.pos.world.uid, material.id, 1)
+        modifyCounters(ctx.ownerUUID, ctx.pos, material.id, 1)
     }
     
     internal fun handleBlockBreak(tileEntity: TileEntity, ctx: BlockBreakContext) {
-        modifyCounters(tileEntity.ownerUUID, ctx.pos.world.uid, tileEntity.material.id, -1)
+        modifyCounters(tileEntity.ownerUUID, ctx.pos, tileEntity.material.id, -1)
     }
     
-    private fun modifyCounters(player: UUID, world: UUID, id: NamespacedId, add: Int) {
+    private fun modifyCounters(player: UUID, pos: BlockPos, id: NamespacedId, add: Int) {
         val playerMap = BLOCK_COUNTER.getOrPut(player, ::HashMap)
         playerMap[id] = max(0, (playerMap[id] ?: 0) + add)
         
-        val playerWorldMap = BLOCK_WORLD_COUNTER.getOrPut(player, ::HashMap).getOrPut(world, ::HashMap)
+        val playerWorldMap = BLOCK_WORLD_COUNTER.getOrPut(player, ::HashMap).getOrPut(pos.world.uid, ::HashMap)
         playerWorldMap[id] = max(0, (playerWorldMap[id] ?: 0) + add)
+        
+        val playerChunkMap = BLOCK_CHUNK_COUNTER.getOrPut(player, ::HashMap).getOrPut(pos.chunkPos, ::HashMap)
+        playerChunkMap[id] = max(0, (playerChunkMap[id] ?: 0) + add)
     }
     
     fun getBlocksPlacedAmount(player: UUID, blockId: NamespacedId): Int {
@@ -50,6 +58,22 @@ internal object TileEntityTracker {
     
     fun getBlocksPlacedAmount(player: UUID, world: UUID, blockId: NamespacedId): Int {
         return BLOCK_WORLD_COUNTER[player]?.get(world)?.get(blockId) ?: 0
+    }
+    
+    fun getBlocksPlacedAmount(player: UUID, chunk: ChunkPos, blockId: NamespacedId): Int {
+        return BLOCK_CHUNK_COUNTER[player]?.get(chunk)?.get(blockId) ?: 0
+    }
+    
+    fun getBlocksPlacedAmount(player: UUID): Int {
+        return BLOCK_COUNTER[player]?.values?.sum() ?: 0
+    }
+    
+    fun getBlocksPlacedAmount(player:UUID, world: UUID): Int {
+        return BLOCK_WORLD_COUNTER[player]?.get(world)?.values?.sum() ?: 0
+    }
+    
+    fun getBlocksPlacedAmount(player:UUID, chunk: ChunkPos): Int {
+        return BLOCK_CHUNK_COUNTER[player]?.get(chunk)?.values?.sum() ?: 0
     }
     
 }
