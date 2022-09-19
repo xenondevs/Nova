b8e8edeb4e90d4279fcfc60f8e2912f3996e955c
ByteZ1337 <0xbytez@gmail.com>
2022-09-19 18:03:59 +0200

    Switch to internal name for MultiTransformer

--- nova/src/main/kotlin/xyz/xenondevs/nova/transformer/MultiTransformer.kt
+++ nova/src/main/kotlin/xyz/xenondevs/nova/transformer/MultiTransformer.kt
@@ -1,10 +1,11 @@
 package xyz.xenondevs.nova.transformer
 
 import xyz.xenondevs.bytebase.jvm.VirtualClassPath
+import xyz.xenondevs.bytebase.util.internalName
 import kotlin.reflect.KClass
 
-internal abstract class MultiTransformer(override val classes: Set<KClass<*>>, override val computeFrames: Boolean): Transformer {
+internal abstract class MultiTransformer(final override val classes: Set<KClass<*>>, override val computeFrames: Boolean): Transformer {
     
-    protected val classWrappers = classes.associate { it.java.name to VirtualClassPath[it] }
+    protected val classWrappers = classes.associate { it.internalName to VirtualClassPath[it] }
     
 }
