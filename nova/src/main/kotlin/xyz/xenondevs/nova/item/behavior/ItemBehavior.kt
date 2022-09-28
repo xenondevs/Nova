b99ec7d8bda6d4899f5cf9995d559d7938b60fde
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2022-09-28 20:47:59 +0200

    ItemBehavior handleDamage, handleBreakBlock, handleAttackEntity

--- nova/src/main/kotlin/xyz/xenondevs/nova/item/behavior/ItemBehavior.kt
+++ nova/src/main/kotlin/xyz/xenondevs/nova/item/behavior/ItemBehavior.kt
@@ -4,10 +4,13 @@ import de.studiocode.invui.item.builder.ItemBuilder
 import org.bukkit.entity.Entity
 import org.bukkit.entity.Player
 import org.bukkit.event.block.Action
+import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.event.entity.EntityDamageByEntityEvent
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.player.PlayerInteractAtEntityEvent
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.player.PlayerItemBreakEvent
+import org.bukkit.event.player.PlayerItemDamageEvent
 import org.bukkit.inventory.ItemStack
 import xyz.xenondevs.nmsutils.network.event.serverbound.ServerboundPlayerActionPacketEvent
 import xyz.xenondevs.nova.item.PacketItemData
@@ -23,6 +26,9 @@ abstract class ItemBehavior : ItemBehaviorHolder<ItemBehavior>() {
     
     open fun handleInteract(player: Player, itemStack: ItemStack, action: Action, event: PlayerInteractEvent) = Unit
     open fun handleEntityInteract(player: Player, itemStack: ItemStack, clicked: Entity, event: PlayerInteractAtEntityEvent) = Unit
+    open fun handleAttackEntity(player: Player, itemStack: ItemStack, attacked: Entity, event: EntityDamageByEntityEvent) = Unit
+    open fun handleBreakBlock(player: Player, itemStack: ItemStack, event: BlockBreakEvent) = Unit
+    open fun handleDamage(player: Player, itemStack: ItemStack, event: PlayerItemDamageEvent) = Unit
     open fun handleBreak(player: Player, itemStack: ItemStack, event: PlayerItemBreakEvent) = Unit
     open fun handleEquip(player: Player, itemStack: ItemStack, equipped: Boolean, event: ArmorEquipEvent) = Unit
     open fun handleInventoryClick(player: Player, itemStack: ItemStack, event: InventoryClickEvent) = Unit
