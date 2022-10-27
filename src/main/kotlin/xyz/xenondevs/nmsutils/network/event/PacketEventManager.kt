a282108c89bc59a8225ca0ae5fc301f452baa5c9
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2022-10-27 17:04:08 +0200

    Replace PacketEventManager synchronization with ReadWriteLock

--- src/main/kotlin/xyz/xenondevs/nmsutils/network/event/PacketEventManager.kt
+++ src/main/kotlin/xyz/xenondevs/nmsutils/network/event/PacketEventManager.kt
@@ -26,6 +26,9 @@ import xyz.xenondevs.nmsutils.network.event.serverbound.ServerboundSetCreativeMo
 import xyz.xenondevs.nmsutils.network.event.serverbound.ServerboundUseItemPacketEvent
 import xyz.xenondevs.nmsutils.util.removeIf
 import java.lang.reflect.Method
+import java.util.concurrent.locks.ReentrantReadWriteLock
+import kotlin.concurrent.read
+import kotlin.concurrent.write
 import kotlin.reflect.KClass
 
 private data class Listener(val instance: Any, val method: Method, val priority: EventPriority, val ignoreIfCancelled: Boolean)
@@ -33,8 +36,9 @@ private data class Listener(val instance: Any, val method: Method, val priority:
 @Suppress("UNCHECKED_CAST")
 object PacketEventManager {
     
-    private val eventTypes = HashMap<KClass<out Packet<*>>, KClass<out PacketEvent<*>>>()
+    private val LOCK = ReentrantReadWriteLock()
     
+    private val eventTypes = HashMap<KClass<out Packet<*>>, KClass<out PacketEvent<*>>>()
     private val eventConstructors = HashMap<KClass<out Packet<*>>, (Packet<*>) -> PacketEvent<Packet<*>>>()
     private val playerEventConstructors = HashMap<KClass<out Packet<*>>, (Player, Packet<*>) -> PlayerPacketEvent<Packet<*>>>()
     
@@ -78,25 +82,25 @@ object PacketEventManager {
         playerEventConstructors[P::class] = constructor as (Player, Packet<*>) -> PlayerPacketEvent<Packet<*>>
     }
     
-    @Synchronized
     internal fun createAndCallEvent(player: Player?, packet: Packet<*>): PacketEvent<*>? {
-        val packetClass = packet::class
-        
-        val packetEventClass = eventTypes[packetClass]
-        if (packetEventClass != null && listeners[packetEventClass] != null) {
-            val event = playerEventConstructors[packetClass]?.invoke(player ?: return null, packet)
-                ?: eventConstructors[packetClass]?.invoke(packet)
-                ?: return null
+        LOCK.read {
+            val packetClass = packet::class
             
-            callEvent(event)
+            val packetEventClass = eventTypes[packetClass]
+            if (packetEventClass != null && listeners[packetEventClass] != null) {
+                val event = playerEventConstructors[packetClass]?.invoke(player ?: return null, packet)
+                    ?: eventConstructors[packetClass]?.invoke(packet)
+                    ?: return null
+                
+                callEvent(event)
+                
+                return event
+            }
             
-            return event
+            return null
         }
-        
-        return null
     }
     
-    @Synchronized
     private fun callEvent(event: PacketEvent<*>) {
         listeners[event::class]?.forEach { (instance, method, _, ignoreIfCancelled) ->
             if (!ignoreIfCancelled || !event.isCancelled) {
@@ -109,46 +113,48 @@ object PacketEventManager {
         }
     }
     
-    @Synchronized
     fun registerListener(listenerInstance: Any) {
-        val instanceListeners = ArrayList<Listener>()
-        
-        listenerInstance::class.java.declaredMethods.forEach { method ->
-            if (method.isAnnotationPresent(PacketHandler::class.java) && method.parameters.size == 1) {
-                val param = method.parameters.first().type.kotlin
-                if (param in eventTypes.values) {
-                    param as KClass<out PacketEvent<*>>
-                    method.isAccessible = true
-                    
-                    val priority = method.getAnnotation(PacketHandler::class.java).priority
-                    val ignoreIfCancelled = method.getAnnotation(PacketHandler::class.java).ignoreIfCancelled
-                    
-                    val listener = Listener(listenerInstance, method, priority, ignoreIfCancelled)
-                    instanceListeners += listener
-                    
-                    val list = listeners[param]?.let(::ArrayList) ?: ArrayList()
-                    list += listener
-                    list.sortBy { it.priority }
-                    
-                    listeners[param] = list
+        LOCK.write {
+            val instanceListeners = ArrayList<Listener>()
+            
+            listenerInstance::class.java.declaredMethods.forEach { method ->
+                if (method.isAnnotationPresent(PacketHandler::class.java) && method.parameters.size == 1) {
+                    val param = method.parameters.first().type.kotlin
+                    if (param in eventTypes.values) {
+                        param as KClass<out PacketEvent<*>>
+                        method.isAccessible = true
+                        
+                        val priority = method.getAnnotation(PacketHandler::class.java).priority
+                        val ignoreIfCancelled = method.getAnnotation(PacketHandler::class.java).ignoreIfCancelled
+                        
+                        val listener = Listener(listenerInstance, method, priority, ignoreIfCancelled)
+                        instanceListeners += listener
+                        
+                        val list = listeners[param]?.let(::ArrayList) ?: ArrayList()
+                        list += listener
+                        list.sortBy { it.priority }
+                        
+                        listeners[param] = list
+                    }
                 }
             }
+            
+            if (instanceListeners.isNotEmpty())
+                listenerInstances[listenerInstance] = instanceListeners
         }
-        
-        if (instanceListeners.isNotEmpty())
-            listenerInstances[listenerInstance] = instanceListeners
     }
     
-    @Synchronized
     fun unregisterListener(listener: Any) {
-        val toRemove = listenerInstances[listener]?.toHashSet() ?: return
-        
-        listeners.removeIf { (_, list) ->
-            list.removeIf { it in toRemove }
-            return@removeIf list.isEmpty()
+        LOCK.write {
+            val toRemove = listenerInstances[listener]?.toHashSet() ?: return
+            
+            listeners.removeIf { (_, list) ->
+                list.removeIf { it in toRemove }
+                return@removeIf list.isEmpty()
+            }
+            
+            listenerInstances -= listener
         }
-        
-        listenerInstances -= listener
     }
     
 }
