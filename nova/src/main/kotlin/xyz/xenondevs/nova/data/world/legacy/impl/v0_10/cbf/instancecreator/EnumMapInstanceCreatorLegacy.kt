66dfe2b557c7518bcfc2a87efb81951e605f6142
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2022-11-12 13:56:34 +0100

    Use enum map constructor from reflection registry

--- nova/src/main/kotlin/xyz/xenondevs/nova/data/world/legacy/impl/v0_10/cbf/instancecreator/EnumMapInstanceCreatorLegacy.kt
+++ nova/src/main/kotlin/xyz/xenondevs/nova/data/world/legacy/impl/v0_10/cbf/instancecreator/EnumMapInstanceCreatorLegacy.kt
@@ -1,13 +1,12 @@
 package xyz.xenondevs.nova.data.world.legacy.impl.v0_10.cbf.instancecreator
 
 import xyz.xenondevs.nova.data.world.legacy.impl.v0_10.cbf.InstanceCreatorLegacy
+import xyz.xenondevs.nova.util.reflection.ReflectionRegistry.ENUM_MAP_CONSTRUCTOR
 import java.lang.reflect.ParameterizedType
 import java.lang.reflect.Type
 import java.util.*
 
 internal object EnumMapInstanceCreatorLegacy : InstanceCreatorLegacy<EnumMap<*, *>> {
-    
-    private val ENUM_MAP_CONSTRUCTOR = EnumMap::class.java.getConstructor(Class::class.java)
     
     override fun createInstance(type: Type): EnumMap<*, *> {
         return ENUM_MAP_CONSTRUCTOR.newInstance((type as ParameterizedType).actualTypeArguments[0] as Class<*>)
