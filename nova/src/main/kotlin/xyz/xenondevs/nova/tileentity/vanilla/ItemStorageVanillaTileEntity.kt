294eaa96d07afb46a8b72f6a4ff483a276f3dd8e
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2022-12-29 14:49:49 +0100

    Fixed #206

--- nova/src/main/kotlin/xyz/xenondevs/nova/tileentity/vanilla/ItemStorageVanillaTileEntity.kt
+++ nova/src/main/kotlin/xyz/xenondevs/nova/tileentity/vanilla/ItemStorageVanillaTileEntity.kt
@@ -3,7 +3,9 @@ package xyz.xenondevs.nova.tileentity.vanilla
 import xyz.xenondevs.nova.data.world.block.state.VanillaTileEntityState
 import xyz.xenondevs.nova.tileentity.network.EndPointDataHolder
 import xyz.xenondevs.nova.tileentity.network.NetworkType
+import xyz.xenondevs.nova.tileentity.network.item.ItemFilter
 import xyz.xenondevs.nova.tileentity.network.item.holder.ItemHolder
+import xyz.xenondevs.nova.util.dropItem
 
 internal abstract class ItemStorageVanillaTileEntity internal constructor(
     blockState: VanillaTileEntityState
@@ -12,6 +14,19 @@ internal abstract class ItemStorageVanillaTileEntity internal constructor(
     abstract val itemHolder: ItemHolder
     final override val holders: MutableMap<NetworkType, EndPointDataHolder>
         by lazy { hashMapOf(NetworkType.ITEMS to itemHolder) }
+    
+    override fun handleRemoved(unload: Boolean) {
+        super.handleRemoved(unload)
+        if (!unload) {
+            val centerLocation = location.clone().add(0.5, 0.0, 0.5)
+            (itemHolder.insertFilters.values.asSequence() + itemHolder.extractFilters.values.asSequence())
+                .map(ItemFilter::createFilterItem)
+                .forEach { centerLocation.dropItem(it) }
+            
+            itemHolder.insertFilters.clear()
+            itemHolder.extractFilters.clear()
+        }
+    }
     
     override fun saveData() {
         super.saveData()
