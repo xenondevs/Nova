e3f9cdaca644cdc140edbbbf84988b79eb04e602
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2023-04-10 21:50:49 +0200

    Fix RegistryBinaryAdapter

--- nova/src/main/kotlin/xyz/xenondevs/nova/registry/RegistryBinaryAdapter.kt
+++ nova/src/main/kotlin/xyz/xenondevs/nova/registry/RegistryBinaryAdapter.kt
@@ -2,7 +2,6 @@ package xyz.xenondevs.nova.registry
 
 import net.minecraft.core.Registry
 import net.minecraft.resources.ResourceLocation
-import xyz.xenondevs.cbf.CBF
 import xyz.xenondevs.cbf.adapter.BinaryAdapter
 import xyz.xenondevs.cbf.io.ByteReader
 import xyz.xenondevs.cbf.io.ByteWriter
@@ -11,13 +10,13 @@ import kotlin.reflect.KType
 internal class RegistryBinaryAdapter<T>(val registry: Registry<T>): BinaryAdapter<T> {
     
     override fun read(type: KType, reader: ByteReader): T {
-        val id = CBF.read<ResourceLocation>(reader)!!
+        val id = ResourceLocation.of(reader.readString(), ':')
         return registry[id]!!
     }
     
     override fun write(obj: T, type: KType, writer: ByteWriter) {
         val id = registry.getKey(obj)!!
-        CBF.write(id, writer)
+        writer.writeString(id.toString())
     }
     
 }
