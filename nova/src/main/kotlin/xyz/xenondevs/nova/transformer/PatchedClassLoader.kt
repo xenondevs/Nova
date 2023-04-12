034d3a0adefb0c084412446863e671b403f61d10
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2023-04-12 16:19:06 +0200

    Only allow minecraft classes to access NovaClassLoader
    Fixes #240

--- nova/src/main/kotlin/xyz/xenondevs/nova/transformer/PatchedClassLoader.kt
+++ nova/src/main/kotlin/xyz/xenondevs/nova/transformer/PatchedClassLoader.kt
@@ -1,8 +1,11 @@
 package xyz.xenondevs.nova.transformer
 
+import org.bukkit.Bukkit
 import xyz.xenondevs.nova.NOVA
 import xyz.xenondevs.nova.loader.NovaClassLoader
 
+// The class loader responsible for loading all bukkit and minecraft classes
+private val SPIGOT_CLASS_LOADER = Bukkit::class.java.classLoader
 
 /**
  * The [PatchedClassLoader] is a class loader that is injected in the class loading hierarchy.
@@ -15,7 +18,7 @@ import xyz.xenondevs.nova.loader.NovaClassLoader
  * the SpigotClassLoader (so a class referenced in Bukkit / NMS code, added in a patch), the class loading process is
  * restarted at the [NovaClassLoader].
  */
-internal class PatchedClassLoader : ClassLoader(NOVA.loader.javaClass.classLoader.parent.parent) {
+internal class PatchedClassLoader : ClassLoader(SPIGOT_CLASS_LOADER.parent) {
     
     private val novaClassLoader = NOVA.javaClass.classLoader as NovaClassLoader
     
@@ -29,7 +32,7 @@ internal class PatchedClassLoader : ClassLoader(NOVA.loader.javaClass.classLoade
         }
         
         // Load class from Nova & libraries
-        if (c == null && checkNonRecursiveStackTrace()) {
+        if (c == null && checkNonRecursive() && checkSpigotLoader()) {
             // Restarts the class loading process at the NovaClassLoader
             return novaClassLoader.loadClass(name, resolve)
         }
@@ -47,14 +50,34 @@ internal class PatchedClassLoader : ClassLoader(NOVA.loader.javaClass.classLoade
     }
     
     /**
-     * Checks that a class load was not caused by Nova or another plugin.
+     * Looks for the NovaClassLoader in the stack trace to prevent recursion.
      */
-    private fun checkNonRecursiveStackTrace(): Boolean {
-        return Thread.currentThread().stackTrace
-            .none {
-                it.className == "org.bukkit.plugin.java.PluginClassLoader"
-                    || it.className == "xyz.xenondevs.nova.loader.NovaClassLoader"
+    private fun checkNonRecursive(): Boolean =
+        Thread.currentThread().stackTrace.none { it.className == "xyz.xenondevs.nova.loader.NovaClassLoader" }
+    
+    /**
+     * Checks that the class initiating the class loading process is loaded by the SpigotClassLoader.
+     */
+    private fun checkSpigotLoader(): Boolean =
+        findLoadingClass().classLoader == SPIGOT_CLASS_LOADER
+    
+    /**
+     * Steps through the stack frames to find the class that triggered the class loading process.
+     */
+    private fun findLoadingClass(): Class<*> {
+        var foundLoadClass = false // used to filter out the methods after loadClass (checkSpigotLoader, findLoadingClass) 
+        var clazz: Class<*>? = null
+        StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE).forEach {
+            if (clazz == null) {
+                if (it.methodName == "loadClass") {
+                    foundLoadClass = true
+                } else if (foundLoadClass) {
+                    clazz = it.declaringClass
+                }
             }
+        }
+        
+        return clazz ?: throw IllegalStateException("Could not find the loading class")
     }
     
 }
