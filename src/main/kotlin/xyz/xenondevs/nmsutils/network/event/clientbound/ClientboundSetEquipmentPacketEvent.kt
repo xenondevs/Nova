f720f060095329a459f76d35294c54d346c0f88c
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2023-04-16 20:02:26 +0200

    Make ClientboundSetEquipmentPacketEvent#slots immutable

--- src/main/kotlin/xyz/xenondevs/nmsutils/network/event/clientbound/ClientboundSetEquipmentPacketEvent.kt
+++ src/main/kotlin/xyz/xenondevs/nmsutils/network/event/clientbound/ClientboundSetEquipmentPacketEvent.kt
@@ -1,6 +1,9 @@
 package xyz.xenondevs.nmsutils.network.event.clientbound
 
+import com.mojang.datafixers.util.Pair
 import net.minecraft.network.protocol.game.ClientboundSetEquipmentPacket
+import net.minecraft.world.entity.EquipmentSlot
+import net.minecraft.world.item.ItemStack
 import org.bukkit.entity.Player
 import xyz.xenondevs.nmsutils.network.event.PlayerPacketEvent
 
@@ -9,8 +12,8 @@ class ClientboundSetEquipmentPacketEvent(
     packet: ClientboundSetEquipmentPacket
 ) : PlayerPacketEvent<ClientboundSetEquipmentPacket>(player, packet) {
     
-    var entity = packet.entity
-    var slots = packet.slots
+    var entity: Int = packet.entity
+    var slots: List<Pair<EquipmentSlot, ItemStack>> = packet.slots
         set(value) {
             field = value
             changed = true
