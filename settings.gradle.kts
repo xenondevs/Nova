70a875afb272d11f4cbeb1d751b274886c419c6f
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2023-05-06 16:52:33 +0200

    Fix #247

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -31,7 +31,7 @@ dependencyResolutionManagement {
             version("spigot", "1.19.4-R0.1-SNAPSHOT")
             version("cbf", "0.5")
             version("xenondevs-commons", "1.1")
-            version("invui", "1.5")
+            version("invui", "1.6")
             
             // plugins
             plugin("kotlin", "org.jetbrains.kotlin.jvm").versionRef("kotlin")
