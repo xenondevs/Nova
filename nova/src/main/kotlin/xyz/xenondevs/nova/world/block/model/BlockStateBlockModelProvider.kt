23d997f89a9bee21a2a0606971d646da281e60db
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2023-05-08 15:34:40 +0200

    Fix backing states remaining in world after explosion
    Removing adjacent block states in the same tick lead to block updates, which then lead to the BackingStateManager placing a backing state again

--- nova/src/main/kotlin/xyz/xenondevs/nova/world/block/model/BlockStateBlockModelProvider.kt
+++ nova/src/main/kotlin/xyz/xenondevs/nova/world/block/model/BlockStateBlockModelProvider.kt
@@ -1,12 +1,14 @@
 package xyz.xenondevs.nova.world.block.model
 
+import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.state.BlockState
-import org.bukkit.Material
 import xyz.xenondevs.nova.data.resources.model.data.BlockStateBlockModelData
 import xyz.xenondevs.nova.data.world.block.property.Directional
 import xyz.xenondevs.nova.data.world.block.state.NovaBlockState
 import xyz.xenondevs.nova.util.getBlockState
 import xyz.xenondevs.nova.util.setBlockStateNoUpdate
+
+private val AIR_BLOCK_STATE = Blocks.AIR.defaultBlockState()
 
 class BlockStateBlockModelProvider(val blockState: NovaBlockState) : BlockModelProvider {
     
@@ -24,7 +26,7 @@ class BlockStateBlockModelProvider(val blockState: NovaBlockState) : BlockModelP
     }
     
     override fun remove(broken: Boolean) {
-        if (broken) pos.block.type = Material.AIR
+        if (broken) pos.setBlockStateNoUpdate(AIR_BLOCK_STATE)
     }
     
     override fun update(subId: Int) {
