5e1f6bbd12603c4ed02f5dfc5f25bfb2d60db291
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2023-08-03 13:05:15 +0200

    Plugin-api methods for retrieving NovaItem from ItemStack

--- nova-api/src/main/java/xyz/xenondevs/nova/api/item/NovaItemRegistry.java
+++ nova-api/src/main/java/xyz/xenondevs/nova/api/item/NovaItemRegistry.java
@@ -1,5 +1,6 @@
 package xyz.xenondevs.nova.api.item;
 
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import xyz.xenondevs.nova.api.data.NamespacedId;
@@ -27,6 +28,15 @@ public interface NovaItemRegistry {
     @NotNull NovaItem get(@NotNull NamespacedId id);
     
     /**
+     * Gets the {@link NovaItem} of the specified {@link ItemStack}.
+     *
+     * @param itemStack The {@link ItemStack} to get the {@link NovaItem} for.
+     * @return The {@link NovaItem} of the specified {@link ItemStack}.
+     * @throws IllegalArgumentException If the {@link ItemStack} is not a Nova item.
+     */
+    @NotNull NovaItem get(@NotNull ItemStack itemStack);
+    
+    /**
      * Gets the {@link NovaItem} with the specified id, or null if there is none.
      *
      * @param id The id of the item.
@@ -41,6 +51,14 @@ public interface NovaItemRegistry {
      * @return The {@link NovaItem} with the specified id, or null if there is none.
      */
     @Nullable NovaItem getOrNull(@NotNull NamespacedId id);
+    
+    /**
+     * Gets the {@link NovaItem} of the specified {@link ItemStack}, or null if it is not a Nova item.
+     *
+     * @param itemStack The {@link ItemStack} to get the {@link NovaItem} for.
+     * @return The {@link NovaItem} of the specified {@link ItemStack}, or null if it is not a Nova item.
+     */
+    @Nullable NovaItem getOrNull(@NotNull ItemStack itemStack);
     
     /**
      * Gets a list of {@link NovaItem NovaItems} with the specified name, ignoring the namespace.
--- nova/src/main/kotlin/xyz/xenondevs/nova/api/ApiItemRegistry.kt
+++ nova/src/main/kotlin/xyz/xenondevs/nova/api/ApiItemRegistry.kt
@@ -1,21 +1,36 @@
 package xyz.xenondevs.nova.api
 
 import net.minecraft.resources.ResourceLocation
+import org.bukkit.inventory.ItemStack
 import xyz.xenondevs.nova.api.data.NamespacedId
 import xyz.xenondevs.nova.api.item.NovaItem
 import xyz.xenondevs.nova.api.item.NovaItemRegistry
 import xyz.xenondevs.nova.registry.NovaRegistries
+import xyz.xenondevs.nova.util.item.novaItem
 
-internal object ApiItemRegistry: NovaItemRegistry {
+internal object ApiItemRegistry : NovaItemRegistry {
     
     override fun getOrNull(id: String): NovaItem? {
-        val loc = ResourceLocation.of(id, ':')
+        val loc = ResourceLocation(id)
         return NovaRegistries.ITEM[loc]?.let(::ApiItemWrapper)
     }
     
-    override fun getOrNull(id: NamespacedId) = getOrNull(id.toString())
-    override fun get(id: String) = getOrNull(id) ?: throw IllegalArgumentException("No block with id $id found!")
-    override fun get(id: NamespacedId) = get(id.toString())
-    override fun getNonNamespaced(name: String) = NovaRegistries.ITEM.getByName(name).map(::ApiItemWrapper)
+    override fun getOrNull(id: NamespacedId): NovaItem? =
+        getOrNull(id.toString())
+    
+    override fun getOrNull(itemStack: ItemStack): NovaItem? =
+        itemStack.novaItem?.let(::ApiItemWrapper)
+    
+    override fun get(id: String): NovaItem =
+        getOrNull(id) ?: throw IllegalArgumentException("No block with id $id found")
+    
+    override fun get(id: NamespacedId): NovaItem =
+        get(id.toString())
+    
+    override fun get(itemStack: ItemStack): NovaItem =
+        itemStack.novaItem?.let(::ApiItemWrapper) ?: throw IllegalArgumentException("ItemStack is not a Nova item")
+    
+    override fun getNonNamespaced(name: String): List<NovaItem> =
+        NovaRegistries.ITEM.getByName(name).map(::ApiItemWrapper)
     
 }
