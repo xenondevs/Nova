481e74b655ce8ed198556da9a00433398cc8b4c0
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2023-09-04 18:52:08 +0200

    Fix TileEntityLimits throwing exception if unconfigured

--- nova/src/main/kotlin/xyz/xenondevs/nova/world/block/limits/TileEntityLimits.kt
+++ nova/src/main/kotlin/xyz/xenondevs/nova/world/block/limits/TileEntityLimits.kt
@@ -1,7 +1,7 @@
 package xyz.xenondevs.nova.world.block.limits
 
 import xyz.xenondevs.nova.data.config.MAIN_CONFIG
-import xyz.xenondevs.nova.data.config.entry
+import xyz.xenondevs.nova.data.config.optionalEntry
 import xyz.xenondevs.nova.util.item.novaItem
 import xyz.xenondevs.nova.world.block.NovaTileEntityBlock
 import xyz.xenondevs.nova.world.block.context.BlockPlaceContext
@@ -9,13 +9,13 @@ import xyz.xenondevs.nova.world.block.limits.BlockLimiter.Companion.ALLOWED
 
 internal object TileEntityLimits {
     
-    private val limiters: List<BlockLimiter> by MAIN_CONFIG.entry<List<BlockLimiter>>("performance", "tile_entity_limits")
+    private val limiters: List<BlockLimiter>? by MAIN_CONFIG.optionalEntry<List<BlockLimiter>>("performance", "tile_entity_limits")
     
     fun canPlace(ctx: BlockPlaceContext): PlaceResult {
         val block = ctx.item.novaItem?.block as? NovaTileEntityBlock
             ?: return ALLOWED
         
-        limiters.forEach {
+        limiters?.forEach {
             val result = it.canPlace(block, ctx)
             if (!result.allowed)
                 return result
