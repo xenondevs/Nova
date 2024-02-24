0f9ef08cd178a60a45c0fa423f88edd1f26d90f8
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2024-02-24 15:49:02 +0100

    Make config provider extension functions config provider functions

--- nova/src/main/kotlin/xyz/xenondevs/nova/data/config/ConfigProvider.kt
+++ nova/src/main/kotlin/xyz/xenondevs/nova/data/config/ConfigProvider.kt
@@ -13,81 +13,9 @@ import kotlin.reflect.KType
 import kotlin.reflect.jvm.javaType
 import kotlin.reflect.typeOf
 
-inline fun <reified T : Any> ConfigProvider.entry(vararg path: String): Provider<T> =
-    entry(typeOf<T>().javaType, *path)
-
-fun <T : Any> ConfigProvider.entry(type: KType, vararg path: String): Provider<T> =
-    entry(type.javaType, *path)
-
-fun <T : Any> ConfigProvider.entry(type: Type, vararg path: String): Provider<T> =
-    map {
-        val node = it.node(*path)
-        if (node.virtual())
-            throw NoSuchElementException("Missing config entry '${path.joinToString(" > ")}' in $relPath")
-        
-        try {
-            return@map node.get(type)!! as T
-        } catch (t: Throwable) {
-            throw IllegalStateException("Config entry '${node.path().joinToString(" > ")}' in $relPath could not be deserialized to $type", t)
-        }
-    }
-
-inline fun <reified T : Any> ConfigProvider.entry(vararg paths: Array<String>): Provider<T> =
-    entry(typeOf<T>().javaType, *paths)
-
-fun <T : Any> ConfigProvider.entry(type: KType, vararg paths: Array<String>): Provider<T> =
-    entry(type.javaType, *paths)
-
-fun <T : Any> ConfigProvider.entry(type: Type, vararg paths: Array<String>): Provider<T> =
-    map {
-        var node: ConfigurationNode? = null
-        for (path in paths) {
-            val possibleNode = it.node(*path)
-            if (!possibleNode.virtual()) {
-                node = possibleNode
-                break
-            }
-        }
-        
-        if (node == null || node.virtual())
-            throw NoSuchElementException("Missing config entry ${paths.joinToString(" or ") { path -> "'${path.joinToString(" > ")}'" }} in $relPath")
-        
-        try {
-            return@map node.get(type)!! as T
-        } catch (t: Throwable) {
-            throw IllegalStateException("Config entry '${node.path().joinToString(" > ")}' in $relPath could not be deserialized to $type", t)
-        }
-    }
-
-inline fun <reified T : Any> ConfigProvider.optionalEntry(vararg path: String): Provider<T?> =
-    optionalEntry(typeOf<T>().javaType, *path)
-
-fun <T : Any> ConfigProvider.optionalEntry(type: KType, vararg path: String): Provider<T?> =
-    optionalEntry(type.javaType, *path)
-
-fun <T : Any> ConfigProvider.optionalEntry(type: Type, vararg path: String): Provider<T?> =
-    map {
-        val node = it.node(*path)
-        if (!node.virtual()) node.get(type) as? T else null
-    }
-
-inline fun <reified T : Any> ConfigProvider.optionalEntry(vararg paths: Array<String>): Provider<T?> =
-    map {
-        var node: ConfigurationNode? = null
-        for (path in paths) {
-            val possibleNode = it.node(*path)
-            if (possibleNode != null) {
-                node = possibleNode
-                break
-            }
-        }
-        
-        node?.get<T?>()
-    }
-
 class ConfigProvider internal constructor(
     private val path: Path,
-    val relPath: String,
+    private val relPath: String,
     private val loadValidation: () -> Boolean
 ) : Provider<CommentedConfigurationNode>() {
     
@@ -97,5 +25,77 @@ class ConfigProvider internal constructor(
         
         return Configs.createLoader(path).load()
     }
+    
+    inline fun <reified T : Any> entry(vararg path: String): Provider<T> =
+        entry(typeOf<T>().javaType, *path)
+    
+    fun <T : Any> entry(type: KType, vararg path: String): Provider<T> =
+        entry(type.javaType, *path)
+    
+    fun <T : Any> entry(type: Type, vararg path: String): Provider<T> =
+        map {
+            val node = it.node(*path)
+            if (node.virtual())
+                throw NoSuchElementException("Missing config entry '${path.joinToString(" > ")}' in $relPath")
+            
+            try {
+                return@map node.get(type)!! as T
+            } catch (t: Throwable) {
+                throw IllegalStateException("Config entry '${node.path().joinToString(" > ")}' in $relPath could not be deserialized to $type", t)
+            }
+        }
+    
+    inline fun <reified T : Any> entry(vararg paths: Array<String>): Provider<T> =
+        entry(typeOf<T>().javaType, *paths)
+    
+    fun <T : Any> entry(type: KType, vararg paths: Array<String>): Provider<T> =
+        entry(type.javaType, *paths)
+    
+    fun <T : Any> entry(type: Type, vararg paths: Array<String>): Provider<T> =
+        map {
+            var node: ConfigurationNode? = null
+            for (path in paths) {
+                val possibleNode = it.node(*path)
+                if (!possibleNode.virtual()) {
+                    node = possibleNode
+                    break
+                }
+            }
+            
+            if (node == null || node.virtual())
+                throw NoSuchElementException("Missing config entry ${paths.joinToString(" or ") { path -> "'${path.joinToString(" > ")}'" }} in $relPath")
+            
+            try {
+                return@map node.get(type)!! as T
+            } catch (t: Throwable) {
+                throw IllegalStateException("Config entry '${node.path().joinToString(" > ")}' in $relPath could not be deserialized to $type", t)
+            }
+        }
+    
+    inline fun <reified T : Any> optionalEntry(vararg path: String): Provider<T?> =
+        optionalEntry(typeOf<T>().javaType, *path)
+    
+    fun <T : Any> optionalEntry(type: KType, vararg path: String): Provider<T?> =
+        optionalEntry(type.javaType, *path)
+    
+    fun <T : Any> optionalEntry(type: Type, vararg path: String): Provider<T?> =
+        map {
+            val node = it.node(*path)
+            if (!node.virtual()) node.get(type) as? T else null
+        }
+    
+    inline fun <reified T : Any> optionalEntry(vararg paths: Array<String>): Provider<T?> =
+        map {
+            var node: ConfigurationNode? = null
+            for (path in paths) {
+                val possibleNode = it.node(*path)
+                if (possibleNode != null) {
+                    node = possibleNode
+                    break
+                }
+            }
+            
+            node?.get<T?>()
+        }
     
 }
