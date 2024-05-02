048496fc8fa799788a78f1657270e79d5387aa80
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2024-05-02 16:56:45 +0200

    Move PacketTask to util package

--- nova/src/main/kotlin/xyz/xenondevs/nova/util/PacketTask.kt
+++ nova/src/main/kotlin/xyz/xenondevs/nova/util/PacketTask.kt
@@ -1,38 +1,49 @@
-package xyz.xenondevs.nova.tileentity
+package xyz.xenondevs.nova.util
 
 import net.minecraft.network.protocol.Packet
 import org.bukkit.entity.Player
 import org.bukkit.scheduler.BukkitTask
-import xyz.xenondevs.nova.util.runTaskTimer
-import xyz.xenondevs.nova.util.send
 
-// TODO: move to different package
+/**
+ * A task that sends the given [packets] to all players from [getViewers]
+ * every [interval] ticks.
+ */
 class PacketTask(
     private val packets: List<Packet<*>>,
     private val interval: Long,
     private val getViewers: () -> Iterable<Player>
-): AutoCloseable {
+) {
     
     private var task: BukkitTask? = null
     
+    /**
+     * Creates a new [PacketTask] that sends the given [packet] to all players from [getViewers].
+     */
+    constructor(packet: Packet<*>, interval: Long, getViewers: () -> Iterable<Player>) : this(listOf(packet), interval, getViewers)
+    
+    /**
+     * Starts the task.
+     */
     fun start() {
         if (task == null)
             task = runTaskTimer(0, interval, ::sendPackets)
     }
     
+    /**
+     * Stops the task.
+     */
     fun stop() {
         task?.cancel()
         task = null
     }
     
-    override fun close() {
-        stop()
-    }
+    /**
+     * Checks whether the task is running.
+     */
+    fun isRunning(): Boolean = task != null
     
     private fun sendPackets() {
         getViewers().forEach { it.send(packets) }
     }
-    
-    fun isRunning() = task != null
     
 }
