d41a03a30a095635e31a63a26efb956c6c2e8520
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2024-05-03 21:13:16 +0200

    Fix ScrollItem always playing click sound

--- nova/src/main/kotlin/xyz/xenondevs/nova/ui/menu/item/ScrollItems.kt
+++ nova/src/main/kotlin/xyz/xenondevs/nova/ui/menu/item/ScrollItems.kt
@@ -5,66 +5,86 @@ import org.bukkit.event.inventory.ClickType
 import org.bukkit.event.inventory.InventoryClickEvent
 import xyz.xenondevs.invui.gui.ScrollGui
 import xyz.xenondevs.invui.item.ItemProvider
-import xyz.xenondevs.invui.item.impl.controlitem.ScrollItem
+import xyz.xenondevs.invui.item.impl.controlitem.ControlItem
 import xyz.xenondevs.nova.item.DefaultGuiItems
 import xyz.xenondevs.nova.util.playClickSound
 
+/**
+ * An ui [ControlItem] for [ScrollGuis][ScrollGui] that scrolls up one line on left-click.
+ */
 class ScrollUpItem(
     private val on: ItemProvider = DefaultGuiItems.ARROW_UP_ON.model.clientsideProvider,
     private val off: ItemProvider = DefaultGuiItems.ARROW_UP_OFF.model.clientsideProvider
-) : ScrollItem(-1) {
+) : ControlItem<ScrollGui<*>>() {
     
     override fun getItemProvider(gui: ScrollGui<*>): ItemProvider =
         if (gui.canScroll(-1)) on else off
     
     override fun handleClick(clickType: ClickType, player: Player, event: InventoryClickEvent) {
-        player.playClickSound()
-        super.handleClick(clickType, player, event)
+        if (clickType == ClickType.LEFT && gui.canScroll(-1)) {
+            player.playClickSound()
+            gui.scroll(-1)
+        }
     }
     
 }
 
+/**
+ * An ui [ControlItem] for [ScrollGuis][ScrollGui] that scrolls down one line on left-click.
+ */
 class ScrollDownItem(
     private val on: ItemProvider = DefaultGuiItems.ARROW_DOWN_ON.model.clientsideProvider,
     private val off: ItemProvider = DefaultGuiItems.ARROW_DOWN_OFF.model.clientsideProvider
-) : ScrollItem(1) {
+) : ControlItem<ScrollGui<*>>() {
     
     override fun getItemProvider(gui: ScrollGui<*>): ItemProvider =
         if (gui.canScroll(1)) on else off
     
     override fun handleClick(clickType: ClickType, player: Player, event: InventoryClickEvent) {
-        player.playClickSound()
-        super.handleClick(clickType, player, event)
+        if (clickType == ClickType.LEFT && gui.canScroll(1)) {
+            player.playClickSound()
+            gui.scroll(1)
+        }
     }
     
 }
 
+/**
+ * An ui [ControlItem] for [ScrollGuis][ScrollGui] that scrolls left one column on left-click.
+ */
 class ScrollLeftItem(
     private val on: ItemProvider = DefaultGuiItems.ARROW_LEFT_ON.model.clientsideProvider,
     private val off: ItemProvider = DefaultGuiItems.ARROW_LEFT_OFF.model.clientsideProvider
-) : ScrollItem(-1) {
+) : ControlItem<ScrollGui<*>>() {
     
     override fun getItemProvider(gui: ScrollGui<*>): ItemProvider =
         if (gui.canScroll(-1)) on else off
     
     override fun handleClick(clickType: ClickType, player: Player, event: InventoryClickEvent) {
-        player.playClickSound()
-        super.handleClick(clickType, player, event)
+        if (clickType == ClickType.LEFT && gui.canScroll(-1)) {
+            player.playClickSound()
+            gui.scroll(-1)
+        }
     }
     
 }
 
+/**
+ * An ui [ControlItem] for [ScrollGuis][ScrollGui] that scrolls right one column on left-click.
+ */
 class ScrollRightItem(
     private val on: ItemProvider = DefaultGuiItems.ARROW_RIGHT_ON.model.clientsideProvider,
     private val off: ItemProvider = DefaultGuiItems.ARROW_RIGHT_OFF.model.clientsideProvider
-) : ScrollItem(1) {
+) : ControlItem<ScrollGui<*>>() {
     
     override fun getItemProvider(gui: ScrollGui<*>): ItemProvider =
         if (gui.canScroll(1)) on else off
     
     override fun handleClick(clickType: ClickType, player: Player, event: InventoryClickEvent) {
-        player.playClickSound()
-        super.handleClick(clickType, player, event)
+        if (clickType == ClickType.LEFT && gui.canScroll(1)) {
+            player.playClickSound()
+            gui.scroll(1)
+        }
     }
     
 }
