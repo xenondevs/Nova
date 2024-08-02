e14f026cdc404e9865ea09825c19ec134fcee1c1
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2024-08-02 19:04:12 +0200

    Delegate waila info of nova note and leave blocks to vanilla info provider

--- nova/src/main/kotlin/xyz/xenondevs/nova/ui/waila/info/impl/DefaultNovaWailaInfoProvider.kt
+++ nova/src/main/kotlin/xyz/xenondevs/nova/ui/waila/info/impl/DefaultNovaWailaInfoProvider.kt
@@ -2,10 +2,8 @@ package xyz.xenondevs.nova.ui.waila.info.impl
 
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
-import net.minecraft.resources.ResourceLocation
 import org.bukkit.entity.Player
 import xyz.xenondevs.commons.collections.firstInstanceOfOrNull
-import xyz.xenondevs.nova.data.resources.lookup.ResourceLookups
 import xyz.xenondevs.nova.tileentity.NetworkedTileEntity
 import xyz.xenondevs.nova.tileentity.network.type.energy.holder.DefaultEnergyHolder
 import xyz.xenondevs.nova.ui.waila.info.NovaWailaInfoProvider
@@ -13,15 +11,25 @@ import xyz.xenondevs.nova.ui.waila.info.WailaInfo
 import xyz.xenondevs.nova.ui.waila.info.WailaLine
 import xyz.xenondevs.nova.ui.waila.info.line.EnergyHolderLine
 import xyz.xenondevs.nova.ui.waila.info.line.ToolLine
-import xyz.xenondevs.nova.util.name
 import xyz.xenondevs.nova.world.BlockPos
+import xyz.xenondevs.nova.world.block.DefaultBlocks
 import xyz.xenondevs.nova.world.block.NovaTileEntityBlock
 import xyz.xenondevs.nova.world.block.state.NovaBlockState
 import xyz.xenondevs.nova.world.format.WorldDataManager
 
+private val DELEGATES = setOf(
+    DefaultBlocks.NOTE_BLOCK, DefaultBlocks.OAK_LEAVES, DefaultBlocks.SPRUCE_LEAVES,
+    DefaultBlocks.BIRCH_LEAVES, DefaultBlocks.JUNGLE_LEAVES, DefaultBlocks.ACACIA_LEAVES,
+    DefaultBlocks.DARK_OAK_LEAVES, DefaultBlocks.MANGROVE_LEAVES, DefaultBlocks.CHERRY_LEAVES,
+    DefaultBlocks.AZALEA_LEAVES, DefaultBlocks.FLOWERING_AZALEA_LEAVES
+)
+
 object DefaultNovaWailaInfoProvider : NovaWailaInfoProvider(null) {
     
     override fun getInfo(player: Player, pos: BlockPos, blockState: NovaBlockState): WailaInfo {
+        if (blockState.block in DELEGATES)
+            return DefaultVanillaWailaInfoProvider.getInfo(player, pos, pos.block.blockData)
+        
         val blockType = blockState.block
         val id = blockType.id
         
