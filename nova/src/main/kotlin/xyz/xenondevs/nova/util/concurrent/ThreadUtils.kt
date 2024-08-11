1095e4b485c75b2019414f2eccd7d1aea108bfd4
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2024-08-11 19:23:27 +0200

    Count watchdog thread as server thread
    If the server is shut down by the watchdog thread, it takes over the role of the server thread and should therefore be considered the server thread. If it is not, Nova will fail to shut down properly due to thread checks in WorldDataManager.

--- nova/src/main/kotlin/xyz/xenondevs/nova/util/concurrent/ThreadUtils.kt
+++ nova/src/main/kotlin/xyz/xenondevs/nova/util/concurrent/ThreadUtils.kt
@@ -1,9 +1,10 @@
 package xyz.xenondevs.nova.util.concurrent
 
+import org.spigotmc.WatchdogThread
 import xyz.xenondevs.nova.util.MINECRAFT_SERVER
 
 internal val isServerThread: Boolean
-    get() = Thread.currentThread() == MINECRAFT_SERVER.serverThread
+    get() = Thread.currentThread() == MINECRAFT_SERVER.serverThread || Thread.currentThread() is WatchdogThread
 
 internal fun checkServerThread() {
     check(isServerThread) { "Not on server thread" }
