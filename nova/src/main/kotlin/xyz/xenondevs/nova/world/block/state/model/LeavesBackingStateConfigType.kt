83aaf8f0c2add7a7f05259e51e70681e80ac7fc0
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2024-08-12 09:54:44 +0200

    Fix all leaves using oak leaves state

--- nova/src/main/kotlin/xyz/xenondevs/nova/world/block/state/model/LeavesBackingStateConfigType.kt
+++ nova/src/main/kotlin/xyz/xenondevs/nova/world/block/state/model/LeavesBackingStateConfigType.kt
@@ -42,55 +42,55 @@ internal class OakLeavesBackingStateConfig(distance: Int, persistent: Boolean) :
     companion object : LeavesBackingStateConfigType<OakLeavesBackingStateConfig>(::OakLeavesBackingStateConfig, "oak_leaves")
 }
 
-internal class SpruceLeavesBackingStateConfig(distance: Int, persistent: Boolean) : LeavesBackingStateConfig(Blocks.OAK_LEAVES, distance, persistent) {
+internal class SpruceLeavesBackingStateConfig(distance: Int, persistent: Boolean) : LeavesBackingStateConfig(Blocks.SPRUCE_LEAVES, distance, persistent) {
     override val type = SpruceLeavesBackingStateConfig
     
     companion object : LeavesBackingStateConfigType<SpruceLeavesBackingStateConfig>(::SpruceLeavesBackingStateConfig, "spruce_leaves")
 }
 
-internal class BirchLeavesBackingStateConfig(distance: Int, persistent: Boolean) : LeavesBackingStateConfig(Blocks.OAK_LEAVES, distance, persistent) {
+internal class BirchLeavesBackingStateConfig(distance: Int, persistent: Boolean) : LeavesBackingStateConfig(Blocks.BIRCH_LEAVES, distance, persistent) {
     override val type = BirchLeavesBackingStateConfig
     
     companion object : LeavesBackingStateConfigType<BirchLeavesBackingStateConfig>(::BirchLeavesBackingStateConfig, "birch_leaves")
 }
 
-internal class JungleLeavesBackingStateConfig(distance: Int, persistent: Boolean) : LeavesBackingStateConfig(Blocks.OAK_LEAVES, distance, persistent) {
+internal class JungleLeavesBackingStateConfig(distance: Int, persistent: Boolean) : LeavesBackingStateConfig(Blocks.JUNGLE_LEAVES, distance, persistent) {
     override val type = JungleLeavesBackingStateConfig
     
     companion object : LeavesBackingStateConfigType<JungleLeavesBackingStateConfig>(::JungleLeavesBackingStateConfig, "jungle_leaves")
 }
 
-internal class AcaciaLeavesBackingStateConfig(distance: Int, persistent: Boolean) : LeavesBackingStateConfig(Blocks.OAK_LEAVES, distance, persistent) {
+internal class AcaciaLeavesBackingStateConfig(distance: Int, persistent: Boolean) : LeavesBackingStateConfig(Blocks.ACACIA_LEAVES, distance, persistent) {
     override val type = AcaciaLeavesBackingStateConfig
     
     companion object : LeavesBackingStateConfigType<AcaciaLeavesBackingStateConfig>(::AcaciaLeavesBackingStateConfig, "acacia_leaves")
 }
 
-internal class DarkOakLeavesBackingStateConfig(distance: Int, persistent: Boolean) : LeavesBackingStateConfig(Blocks.OAK_LEAVES, distance, persistent) {
+internal class DarkOakLeavesBackingStateConfig(distance: Int, persistent: Boolean) : LeavesBackingStateConfig(Blocks.DARK_OAK_LEAVES, distance, persistent) {
     override val type = DarkOakLeavesBackingStateConfig
     
     companion object : LeavesBackingStateConfigType<DarkOakLeavesBackingStateConfig>(::DarkOakLeavesBackingStateConfig, "dark_oak_leaves")
 }
 
-internal class MangroveLeavesBackingStateConfig(distance: Int, persistent: Boolean) : LeavesBackingStateConfig(Blocks.OAK_LEAVES, distance, persistent) {
+internal class MangroveLeavesBackingStateConfig(distance: Int, persistent: Boolean) : LeavesBackingStateConfig(Blocks.MANGROVE_LEAVES, distance, persistent) {
     override val type = MangroveLeavesBackingStateConfig
     
     companion object : LeavesBackingStateConfigType<MangroveLeavesBackingStateConfig>(::MangroveLeavesBackingStateConfig, "mangrove_leaves")
 }
 
-internal class CherryLeavesBackingStateConfig(distance: Int, persistent: Boolean) : LeavesBackingStateConfig(Blocks.OAK_LEAVES, distance, persistent) {
+internal class CherryLeavesBackingStateConfig(distance: Int, persistent: Boolean) : LeavesBackingStateConfig(Blocks.CHERRY_LEAVES, distance, persistent) {
     override val type = CherryLeavesBackingStateConfig
     
     companion object : LeavesBackingStateConfigType<CherryLeavesBackingStateConfig>(::CherryLeavesBackingStateConfig, "cherry_leaves")
 }
 
-internal class AzaleaLeavesBackingStateConfig(distance: Int, persistent: Boolean) : LeavesBackingStateConfig(Blocks.OAK_LEAVES, distance, persistent) {
+internal class AzaleaLeavesBackingStateConfig(distance: Int, persistent: Boolean) : LeavesBackingStateConfig(Blocks.AZALEA_LEAVES, distance, persistent) {
     override val type = AzaleaLeavesBackingStateConfig
     
     companion object : LeavesBackingStateConfigType<AzaleaLeavesBackingStateConfig>(::AzaleaLeavesBackingStateConfig, "azalea_leaves")
 }
 
-internal class FloweringAzaleaLeavesBackingStateConfig(distance: Int, persistent: Boolean) : LeavesBackingStateConfig(Blocks.OAK_LEAVES, distance, persistent) {
+internal class FloweringAzaleaLeavesBackingStateConfig(distance: Int, persistent: Boolean) : LeavesBackingStateConfig(Blocks.FLOWERING_AZALEA_LEAVES, distance, persistent) {
     override val type = FloweringAzaleaLeavesBackingStateConfig
     
     companion object : LeavesBackingStateConfigType<FloweringAzaleaLeavesBackingStateConfig>(::FloweringAzaleaLeavesBackingStateConfig, "flowering_azalea_leaves")
