59e484520a6c6c0ab8f78d9ac4424918f2822b62
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2024-11-15 19:10:30 +0100

    Update ResourceGeneration.kt

--- nova/src/main/kotlin/xyz/xenondevs/nova/resources/ResourceGeneration.kt
+++ nova/src/main/kotlin/xyz/xenondevs/nova/resources/ResourceGeneration.kt
@@ -23,7 +23,6 @@ import xyz.xenondevs.nova.world.item.DefaultBlockOverlays
 import xyz.xenondevs.nova.world.item.DefaultGuiItems
 import xyz.xenondevs.nova.world.item.DefaultItems
 import java.security.MessageDigest
-import kotlin.io.path.notExists
 
 private const val FORCE_REBUILD_FLAG = "NovaForceRegenerateResourcePack"
 private const val VERSION_HASH = "version_hash"
@@ -55,7 +54,7 @@ internal object ResourceGeneration {
         private fun init() {
             versionHash = calculateVersionHash()
             if (System.getProperty(FORCE_REBUILD_FLAG) != null
-                || ResourcePackBuilder.RESOURCE_PACK_FILE.notExists()
+                || !ResourcePackBuilder.RESOURCE_PACK_FILE.exists()
                 || PermanentStorage.retrieveOrNull<String>(VERSION_HASH) != versionHash
                 || !ResourceLookups.hasAllLookups()
                 || !ResourceLookups.tryLoadAll()
