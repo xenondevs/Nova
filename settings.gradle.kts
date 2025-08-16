5096e4c9ddf81d7087e355619dba6092221bac80
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2025-08-16 14:36:08 +0200

    Update origami

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -34,7 +34,7 @@ dependencyResolutionManagement {
     versionCatalogs {
         create("libs")
         create("origamiLibs") {
-            from("xyz.xenondevs.origami:origami-catalog:0.1.1")
+            from("xyz.xenondevs.origami:origami-catalog:0.1.2")
         }
     }
 }
