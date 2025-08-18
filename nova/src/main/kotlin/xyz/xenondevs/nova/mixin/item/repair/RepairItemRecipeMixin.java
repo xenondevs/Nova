fe8c5f22e313304e6dfeff44c8d5e8da1e3702fe
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2025-08-18 11:35:09 +0200

    Fix RepairItemRecipe accepting non-repairable items

--- nova/src/main/kotlin/xyz/xenondevs/nova/mixin/item/repair/RepairItemRecipeMixin.java
+++ nova/src/main/kotlin/xyz/xenondevs/nova/mixin/item/repair/RepairItemRecipeMixin.java
@@ -1,5 +1,6 @@
 package xyz.xenondevs.nova.mixin.item.repair;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RepairItemRecipe;
@@ -7,20 +8,29 @@ import net.minecraft.world.level.ItemLike;
 import org.bukkit.craftbukkit.inventory.CraftItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xyz.xenondevs.nova.util.item.ItemUtilsKt;
 
 @Mixin(RepairItemRecipe.class)
 abstract class RepairItemRecipeMixin {
     
-    @Inject(method = "canCombine", at = @At("HEAD"), cancellable = true)
-    private static void canCombine(ItemStack first, ItemStack second, CallbackInfoReturnable<Boolean> cir) {
+    @ModifyExpressionValue(
+        method = "canCombine",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"
+        )
+    )
+    private static boolean sameType(
+        boolean original,
+        @Local(argsOnly = true, ordinal = 0) ItemStack first,
+        @Local(argsOnly = true, ordinal = 1) ItemStack second
+    ) {
         var firstNovaItem = ItemUtilsKt.getNovaItem(first);
         var secondNovaItem = ItemUtilsKt.getNovaItem(second);
         if (firstNovaItem != null || secondNovaItem != null)
-            cir.setReturnValue(firstNovaItem == secondNovaItem);
+            return firstNovaItem == secondNovaItem;
+        return original;
     }
     
     @Redirect(
