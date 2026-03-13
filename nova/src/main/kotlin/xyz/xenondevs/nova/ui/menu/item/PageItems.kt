b7cb06fd954128f10201c682337e24a2562f7314
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2026-03-13 13:53:16 +0100

    Add DSL-friendly page items

--- nova/src/main/kotlin/xyz/xenondevs/nova/ui/menu/item/PageItems.kt
+++ nova/src/main/kotlin/xyz/xenondevs/nova/ui/menu/item/PageItems.kt
@@ -4,16 +4,85 @@ import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.entity.Player
 import org.bukkit.event.inventory.ClickType
+import xyz.xenondevs.commons.provider.MutableProvider
 import xyz.xenondevs.commons.provider.Provider
+import xyz.xenondevs.commons.provider.combinedProvider
 import xyz.xenondevs.commons.provider.provider
 import xyz.xenondevs.invui.Click
+import xyz.xenondevs.invui.dsl.item
 import xyz.xenondevs.invui.gui.PagedGui
 import xyz.xenondevs.invui.item.AbstractPagedGuiBoundItem
+import xyz.xenondevs.invui.item.Item
 import xyz.xenondevs.invui.item.ItemBuilder
 import xyz.xenondevs.invui.item.ItemProvider
 import xyz.xenondevs.nova.util.playClickSound
 import xyz.xenondevs.nova.world.item.DefaultGuiItems
 import xyz.xenondevs.nova.world.item.clientsideProvider
+
+/**
+ * A UI item for paged guis that goes back one page on left-click.
+ * Uses [on] as item provider if it is possible to go back, otherwise uses [off].
+ */
+fun pageBackItem(
+    page: MutableProvider<Int>,
+    on: Provider<ItemProvider> = DefaultGuiItems.TP_ARROW_LEFT_ON.clientsideProvider,
+    off: Provider<ItemProvider> = DefaultGuiItems.TP_ARROW_LEFT_OFF.clientsideProvider
+): Item = pageBackItem(
+    page,
+    combinedProvider(
+        page, on, off
+    ) { page, on, off -> if (page > 0) on else off }
+)
+
+/**
+ * A UI item for paged guis that goes back one page on left-click.
+ */
+fun pageBackItem(
+    page: MutableProvider<Int>,
+    itemProvider: Provider<ItemProvider>
+): Item = item {
+    this.itemProvider by itemProvider
+    onClick {
+        if (clickType == ClickType.LEFT && page.get() > 0) {
+            player.playClickSound()
+            page.set(page.get() - 1)
+        }
+    }
+}
+
+/**
+ * A UI item for paged guis that goes forward one page on left-click.
+ * Uses [on] as item provider if it is possible to go forward, otherwise uses [off].
+ */
+fun pageForwardItem(
+    page: MutableProvider<Int>,
+    pageCount: Provider<Int>,
+    on: Provider<ItemProvider> = DefaultGuiItems.TP_ARROW_RIGHT_ON.clientsideProvider,
+    off: Provider<ItemProvider> = DefaultGuiItems.TP_ARROW_RIGHT_OFF.clientsideProvider
+): Item = pageForwardItem(
+    page,
+    pageCount,
+    combinedProvider(
+        page, pageCount, on, off
+    ) { page, pageCount, on, off -> if (page + 1 < pageCount) on else off }
+)
+
+/**
+ * A UI item for paged guis that goes forward one page on left-click.
+ */
+fun pageForwardItem(
+    page: MutableProvider<Int>,
+    pageCount: Provider<Int>,
+    itemProvider: Provider<ItemProvider>
+): Item = item {
+    this.itemProvider by itemProvider
+    onClick {
+        if (clickType == ClickType.LEFT && page.get() + 1 < pageCount.get()) {
+            player.playClickSound()
+            page.set(page.get() + 1)
+        }
+    }
+}
 
 /**
  * A UI item for [PagedGuis][PagedGui] that goes back one page on left-click.
