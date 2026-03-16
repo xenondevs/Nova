a02de7e9dcb209d0ef53ca4832cef386be3b0996
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2026-03-16 17:00:10 +0100

    Add DSL-friendly back item

--- nova/src/main/kotlin/xyz/xenondevs/nova/ui/menu/item/BackItem.kt
+++ nova/src/main/kotlin/xyz/xenondevs/nova/ui/menu/item/BackItem.kt
@@ -5,12 +5,28 @@ import org.bukkit.event.inventory.ClickType
 import xyz.xenondevs.commons.provider.Provider
 import xyz.xenondevs.commons.provider.provider
 import xyz.xenondevs.invui.Click
+import xyz.xenondevs.invui.dsl.item
 import xyz.xenondevs.invui.item.AbstractItem
+import xyz.xenondevs.invui.item.Item
 import xyz.xenondevs.invui.item.ItemProvider
 import xyz.xenondevs.invui.window.Window
 import xyz.xenondevs.nova.util.playClickSound
 import xyz.xenondevs.nova.world.item.DefaultGuiItems
 import xyz.xenondevs.nova.world.item.clientsideProvider
+
+/**
+ * A UI item that opens [previousWindow] when clicked.
+ */
+fun backItem(
+    previousWindow: Provider<Window>,
+    itemProvider: Provider<ItemProvider> = DefaultGuiItems.TP_ARROW_LEFT_ON.clientsideProvider,
+): Item = item {
+    this.itemProvider by itemProvider
+    onClick { 
+        player.playClickSound()
+        previousWindow.get().open()
+    }
+}
 
 class BackItem(
     private val itemProvider: Provider<ItemProvider> = DefaultGuiItems.ARROW_LEFT_ON.clientsideProvider,
