4b22c6e533ed82170b37aeeb895208b5e929d962
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2026-03-19 12:07:26 +0100

    DSL-friendly vertical bar items

--- nova/src/main/kotlin/xyz/xenondevs/nova/ui/menu/EnergyBar.kt
+++ nova/src/main/kotlin/xyz/xenondevs/nova/ui/menu/EnergyBar.kt
@@ -3,13 +3,81 @@ package xyz.xenondevs.nova.ui.menu
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import xyz.xenondevs.commons.provider.Provider
+import xyz.xenondevs.commons.provider.combinedProvider
+import xyz.xenondevs.invui.dsl.ClickDsl
+import xyz.xenondevs.invui.dsl.property.by
+import xyz.xenondevs.invui.gui.SlotElementSupplier
 import xyz.xenondevs.invui.item.Item
-import xyz.xenondevs.invui.item.setItemProvider
 import xyz.xenondevs.nova.util.NumberFormatUtils
 import xyz.xenondevs.nova.world.block.tileentity.network.type.energy.EnergyNetwork
 import xyz.xenondevs.nova.world.block.tileentity.network.type.energy.holder.DefaultEnergyHolder
 import xyz.xenondevs.nova.world.item.DefaultGuiItems
 import xyz.xenondevs.nova.world.item.NovaItem
+
+/**
+ * A [verticalBar] for displaying an energy level.
+ * Ranges from [DefaultEnergyHolder.energyProvider] to [DefaultEnergyHolder.maxEnergyProvider].
+ * Shows [DefaultEnergyHolder.energyPlusProvider] and [DefaultEnergyHolder.energyMinusProvider] in the lore.
+ * Uses [barType] as the bar item type.
+ */
+fun energyBar(
+    energyHolder: DefaultEnergyHolder,
+    barType: Provider<NovaItem> = DefaultGuiItems.TP_BAR_RED,
+    onClick: ClickDsl.() -> Unit = {}
+): SlotElementSupplier = energyBar(
+    energyHolder.energyProvider,
+    energyHolder.maxEnergyProvider,
+    energyHolder.energyPlusProvider,
+    energyHolder.energyMinusProvider,
+    barType,
+    onClick
+)
+
+/**
+ * A [verticalBar] for displaying an energy level.
+ * Ranges from [energy] to [maxEnergy].
+ * Shows [energyPlus] and [energyMinus] in the lore.
+ * Uses [barType] as the bar item type.
+ */
+fun energyBar(
+    energy: Provider<Long>,
+    maxEnergy: Provider<Long>,
+    energyPlus: Provider<Long>,
+    energyMinus: Provider<Long>,
+    barType: Provider<NovaItem> = DefaultGuiItems.TP_BAR_RED,
+    onClick: ClickDsl.() -> Unit = {}
+): SlotElementSupplier = verticalBar(
+    percentage = combinedProvider(energy, maxEnergy) { energy, maxEnergy -> energy.toDouble() / maxEnergy.toDouble() },
+    barType = barType,
+    modifyItemProvider = {
+        name by combinedProvider(energy, maxEnergy) { energy, maxEnergy ->
+            when {
+                energy == Long.MAX_VALUE -> "∞ J / ∞ J"
+                else -> NumberFormatUtils.getEnergyString(energy, maxEnergy)
+            }
+        }
+        lore by combinedProvider(energyPlus, energyMinus, EnergyNetwork.TICK_DELAY_PROVIDER) { energyPlus, energyMinus, tickDelay ->
+            buildList {
+                if (energyPlus > 0) {
+                    this += Component.translatable(
+                        "menu.nova.energy_per_tick",
+                        NamedTextColor.GRAY,
+                        Component.text("+" + NumberFormatUtils.getEnergyString(energyPlus / tickDelay))
+                    )
+                }
+                
+                if (energyMinus > 0) {
+                    this += Component.translatable(
+                        "menu.nova.energy_per_tick",
+                        NamedTextColor.GRAY,
+                        Component.text("-" + NumberFormatUtils.getEnergyString(energyMinus / tickDelay))
+                    )
+                }
+            }
+        }
+    },
+    onClick = onClick
+)
 
 /**
  * A multi-item gui component for displaying energy levels.
@@ -33,13 +101,13 @@ class EnergyBar(
     override fun createBarItem(section: Int): Item =
         Item.builder()
             .updatePeriodically(1)
-            .setItemProvider(item) { item ->
+            .setItemProvider {
                 val energy = energy.get()
                 val maxEnergy = maxEnergy.get()
                 val energyPlus = getEnergyPlus()
                 val energyMinus = getEnergyMinus()
                 
-                val builder = createItemBuilder(item, section, energy.toDouble() / maxEnergy.toDouble())
+                val builder = createItemBuilder(item.get(), section, energy.toDouble() / maxEnergy.toDouble())
                 
                 if (energy == Long.MAX_VALUE) {
                     builder.setName("∞ J / ∞ J")
--- nova/src/main/kotlin/xyz/xenondevs/nova/world/block/tileentity/network/type/energy/EnergyNetworkGroup.kt
+++ nova/src/main/kotlin/xyz/xenondevs/nova/world/block/tileentity/network/type/energy/EnergyNetworkGroup.kt
@@ -2,11 +2,27 @@ package xyz.xenondevs.nova.world.block.tileentity.network.type.energy
 
 import xyz.xenondevs.nova.world.block.tileentity.network.NetworkGroup
 import xyz.xenondevs.nova.world.block.tileentity.network.NetworkGroupData
+import xyz.xenondevs.nova.world.block.tileentity.network.node.NetworkEndPoint
+import xyz.xenondevs.nova.world.block.tileentity.network.node.NetworkNodeConnection
+import xyz.xenondevs.nova.world.block.tileentity.network.type.energy.holder.DefaultEnergyHolder
 
 internal class EnergyNetworkGroup(data: NetworkGroupData<EnergyNetwork>) : NetworkGroup<EnergyNetwork>, NetworkGroupData<EnergyNetwork> by data {
     
+    private val defaultHolders: Set<DefaultEnergyHolder> = networks.asSequence()
+        .flatMap { it.nodes.values.asSequence().map(NetworkNodeConnection::node)  }
+        .filterIsInstance<NetworkEndPoint>()
+        .flatMap { it.holders.asSequence() }
+        .filterIsInstance<DefaultEnergyHolder>()
+        .toHashSet()
+    
     override fun tick() {
         networks.forEach(EnergyNetwork::tick)
+    }
+    
+    override fun postTick() {
+        for (energyHolder in defaultHolders) {
+            energyHolder.postTick()
+        }
     }
     
 }
