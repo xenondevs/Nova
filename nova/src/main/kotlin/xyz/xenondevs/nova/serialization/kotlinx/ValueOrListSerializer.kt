0c8b901bc5d6a7536543b694f288c4469b822205
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2026-03-29 20:16:45 +0200

    Custom item categories with description

--- nova/src/main/kotlin/xyz/xenondevs/nova/serialization/kotlinx/ValueOrListSerializer.kt
+++ nova/src/main/kotlin/xyz/xenondevs/nova/serialization/kotlinx/ValueOrListSerializer.kt
@@ -1,10 +1,13 @@
 package xyz.xenondevs.nova.serialization.kotlinx
 
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonTransformingSerializer
+
+internal typealias ValueOrList<T> = @Serializable(with = ValueOrListSerializer::class) List<T>
 
 internal class ValueOrListSerializer<T>(dataSerializer: KSerializer<T>) : JsonTransformingSerializer<List<T>>(ListSerializer(dataSerializer)) {
     
