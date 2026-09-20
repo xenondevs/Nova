d79a9a8a742b55d4da1d6a879a2f46f02f34099f
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2026-09-20 16:47:38 +0200

    Modernize Damageable behavior
    This removes itemDamageOnAttackEntity and itemDamageOnBreakBlock which now live the Weapon / Tool behavior respectively. The repair ingredient now uses a registry entry set instead of a recipe choice and uses the appropriate data component.

--- nova/src/main/kotlin/xyz/xenondevs/nova/world/item/behavior/Damageable.kt
+++ nova/src/main/kotlin/xyz/xenondevs/nova/world/item/behavior/Damageable.kt
@@ -3,18 +3,19 @@
 package xyz.xenondevs.nova.world.item.behavior
 
 import io.papermc.paper.datacomponent.DataComponentTypes
-import io.papermc.paper.datacomponent.item.Weapon.weapon
+import io.papermc.paper.datacomponent.item.Repairable
 import io.papermc.paper.registry.keys.SoundEventKeys
 import net.kyori.adventure.key.Key
 import org.bukkit.Registry
 import org.bukkit.Sound
 import org.bukkit.inventory.ItemStack
-import org.bukkit.inventory.RecipeChoice
+import org.bukkit.inventory.ItemType
 import xyz.xenondevs.commons.provider.Provider
 import xyz.xenondevs.commons.provider.mapNonNull
 import xyz.xenondevs.commons.provider.orElse
 import xyz.xenondevs.nova.config.entry
 import xyz.xenondevs.nova.config.optionalEntry
+import xyz.xenondevs.nova.registry.RegistryEntrySet
 import xyz.xenondevs.nova.world.item.DataComponentMap
 import xyz.xenondevs.nova.world.item.buildDataComponentMapProvider
 
@@ -24,14 +25,6 @@ import xyz.xenondevs.nova.world.item.buildDataComponentMapProvider
  * @param maxDurability The maximum durability of the item.
  * Defaults to `1`.
  * Used when `max_durability` is not specified in the item's config.
- *
- * @param itemDamageOnAttackEntity The amount of damage the item receives when attacking an entity.
- * Defaults to `1`.
- * Used when `item_damage_on_attack_entity` is not specified in the item's config.
- *
- * @param itemDamageOnBreakBlock The amount of damage the item receives when breaking a block.
- * Defaults to `1`.
- * Used when `item_damage_on_break_block` is not specified in the item's config.
  *
  * @param repairIngredient The ingredient required to repair the item. Can be null for items that cannot be repaired.
  * Defaults to `null`.
@@ -43,16 +36,12 @@ import xyz.xenondevs.nova.world.item.buildDataComponentMapProvider
  */
 fun Damageable(
     maxDurability: Int = 1,
-    itemDamageOnAttackEntity: Int = 1,
-    itemDamageOnBreakBlock: Int = 1,
-    repairIngredient: RecipeChoice? = null,
+    repairIngredient: RegistryEntrySet.Paper<ItemType>? = null,
     breakSound: Key = SoundEventKeys.ENTITY_ITEM_BREAK.key()
 ) = ItemBehaviorFactory { _, cfg ->
     Damageable(
         cfg.entry(maxDurability, listOf("max_durability"), listOf("durability")),
-        cfg.entry(itemDamageOnAttackEntity, "item_damage_on_attack_entity"),
-        cfg.entry(itemDamageOnBreakBlock, "item_damage_on_break_block"),
-        cfg.optionalEntry<RecipeChoice>("repair_ingredient").orElse(repairIngredient),
+        cfg.optionalEntry<RegistryEntrySet.Paper<ItemType>>("repair_ingredient").orElse(repairIngredient),
         cfg.entry(breakSound, "break_sound")
     )
 }
@@ -68,9 +57,7 @@ fun Damageable(
  */
 class Damageable(
     maxDurability: Provider<Int>,
-    itemDamageOnAttackEntity: Provider<Int>,
-    itemDamageOnBreakBlock: Provider<Int>,
-    repairIngredient: Provider<RecipeChoice?>,
+    repairIngredient: Provider<RegistryEntrySet.Paper<ItemType>?>,
     breakSound: Provider<Key>
 ) : ItemBehavior {
     
@@ -80,19 +67,9 @@ class Damageable(
     val maxDurability: Int by maxDurability
     
     /**
-     * The amount of damage the item receives when attacking an entity.
-     */
-    val itemDamageOnAttackEntity: Int by itemDamageOnAttackEntity
-    
-    /**
-     * The amount of damage the item receives when breaking a block.
-     */
-    val itemDamageOnBreakBlock: Int by itemDamageOnBreakBlock
-    
-    /**
      * The ingredient required to repair the item in an anvil.
      */
-    val repairIngredient: RecipeChoice? by repairIngredient
+    val repairIngredient: RegistryEntrySet.Paper<ItemType>? by repairIngredient
     
     /**
      * The key of the sound that is played when the item breaks.
@@ -109,22 +86,13 @@ class Damageable(
         this[DataComponentTypes.MAX_DAMAGE] = maxDurability
         this[DataComponentTypes.DAMAGE] = 0
         this[DataComponentTypes.BREAK_SOUND] = breakSound
-        this[DataComponentTypes.WEAPON] = itemDamageOnAttackEntity
-            .map { it.takeUnless { it < 0 } }
-            .mapNonNull {
-                weapon()
-                    .itemDamagePerAttack(it)
-                    .disableBlockingForSeconds(0f) // use the lowest value for merging with weapon component of Tool
-                    .build()
-            }
+        this[DataComponentTypes.REPAIRABLE] = repairIngredient.mapNonNull { Repairable.repairable(it.toRegistryKeySet()) }
     }
     
     override fun toString(itemStack: ItemStack): String {
         return "Damageable(" +
             "damage=${itemStack.getData(DataComponentTypes.DAMAGE) ?: 0}, " +
             "maxDurability=$maxDurability, " +
-            "itemDamageOnAttackEntity=$itemDamageOnAttackEntity, " +
-            "itemDamageOnBreakBlock=$itemDamageOnBreakBlock, " +
             "repairIngredient=$repairIngredient" +
             "breakSound=${breakSoundKey.asString()}" +
             ")"
